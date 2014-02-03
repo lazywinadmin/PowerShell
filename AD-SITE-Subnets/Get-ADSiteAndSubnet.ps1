3b65bd205a670f508924adfd8bec736f088d94f6
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2014-02-03 18:48:19 -0500

    Upload Get-AdSiteAndSubnet

---
+++ AD-SITE-Subnets/Get-ADSiteAndSubnet.ps1
@@ -0,0 +1,80 @@
+﻿function Get-ADSiteAndSubnet {
+<#
+	.SYNOPSIS
+		This function will retrieve Site names, subnets names and descriptions.
+
+	.DESCRIPTION
+		This function will retrieve Site names, subnets names and descriptions.
+
+	.EXAMPLE
+		Get-ADSiteAndSubnet
+	
+	.EXAMPLE
+		Get-ADSiteAndSubnet | Export-Csv -Path .\ADSiteInventory.csv
+
+	.OUTPUTS
+		PSObject
+
+	.NOTES
+		AUTHOR	: Francois-Xavier Cat
+		DATE	: 2014/02/03
+		
+		HISTORY	:
+	
+			1.0		2014/02/03	Initial Version
+			
+	
+#>
+	[CmdletBinding()]
+    PARAM()
+    BEGIN {Write-Verbose -Message "[BEGIN] Starting Script..."}
+    PROCESS
+    {
+		TRY{
+	        # Domain and Sites Information
+	        $Forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+	        $SiteInfo = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().Sites
+
+	        # Forest Context
+	        $ForestType = [System.DirectoryServices.ActiveDirectory.DirectoryContexttype]"forest"
+	        $ForestContext = New-Object -TypeName System.DirectoryServices.ActiveDirectory.DirectoryContext -ArgumentList $ForestType,$Forest
+            
+            # Distinguished Name of the Configuration Partition
+            $Configuration = ([ADSI]"LDAP://RootDSE").configurationNamingContext
+
+            # Get the Subnet Container
+            $SubnetsContainer = [ADSI]"LDAP://CN=Subnets,CN=Sites,$Configuration"
+            $SubnetsContainerchildren = $SubnetsContainer.Children
+
+	        FOREACH ($item in $SiteInfo){
+				
+				Write-Verbose -Message "[PROCESS] SITE: $($item.name)"
+
+                $output = @{
+                    Name = $item.name
+                }
+                    FOREACH ($i in $item.Subnets.name){
+                        Write-verbose -message "[PROCESS] SUBNET: $i"
+                        $output.Subnet = $i
+                        $SubnetAdditionalInfo = $SubnetsContainerchildren.Where({$_.name -match $i})
+
+                        Write-verbose -message "[PROCESS] SUBNET: $i - DESCRIPTION: $($SubnetAdditionalInfo.Description)"
+                        $output.Description = $($SubnetAdditionalInfo.Description)
+                        
+                        Write-verbose -message "[PROCESS] OUTPUT INFO"
+
+                        New-Object -TypeName PSObject -Property $output
+                    }
+	        }#Foreach ($item in $SiteInfo)
+		}#TRY
+		CATCH
+		{
+			Write-Warning -Message "[PROCESS] Something Wrong Happened"
+			Write-Warning -Message $Error[0]
+		}#CATCH
+    }#PROCESS
+    END
+	{
+		Write-Verbose -Message "[END] Script Completed!"
+	}#END
+}#get-ADSiteServicesInfo
