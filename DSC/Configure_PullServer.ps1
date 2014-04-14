a0037fb6868476ccd60e04449dd75599ba1f99b0
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2014-04-13 20:28:46 -0400

    Upload DSC test

---
+++ DSC/Configure_PullServer.ps1
@@ -0,0 +1,44 @@
+﻿Configuration CreatePullServer
+{
+    [CmdletBinding()]
+    Param(
+        [String[]]$ComputerName = $env:COMPUTERNAME   
+    )
+    
+    Import-DSCResource -ModuleName xPSDesiredStateConfiguration
+
+    Node $ComputerName
+    {
+
+        WindowsFeature DSCServiceFeature
+        {
+            Ensure = "Present"
+            Name = "DSC-Service"
+        }
+
+        xDscWebService PSDSCPullServer
+        {
+            Ensure = "Present"
+            EndpointName = "PSDSCPullServer"
+            Port = 8080
+            PhysicalPath = "$env:SystemDrive\inetpub\wwwroot\PSDSCPullServer"
+            CertificateThumbPrint = "AllowUnencryptedTraffic"
+            ModulePath = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules"
+            ConfigurationPath = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration"
+            State = "Started"
+            DependsOn = "[WindowsFeature]DSCServiceFeature"
+        }
+
+        xDscWebService PSDSCComplianceServer
+        {
+            Ensure = "Present"
+            EndpointName = "PSDSCComplianceServer"
+            Port = 9080
+            PhysicalPath = "$env:SystemDrive\inetpub\wwwroot\PSDSCComplianceServer"
+            CertificateThumbPrint = "AllowUnencryptedTraffic"
+            State = "Started"
+            IsComplianceServer = $true
+            DependsOn = ("[WindowsFeature]DSCServiceFeature", "[xDSCWebService]PSDSCPullServer")
+        }
+    }#Node
+}#Configuration
