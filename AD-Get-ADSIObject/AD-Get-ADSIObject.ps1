c956f38b71418701d9fde9cd51774acb9c960c04
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2014-05-04 17:57:51 -0400

    Update Get-ADSIObject

--- AD-Get-ADSIObject/AD-Get-ADSIObject.ps1
+++ AD-Get-ADSIObject/AD-Get-ADSIObject.ps1
@@ -17,39 +17,52 @@
 		Check info from http://mow001.blogspot.ca/2006/07/powershell-and-active-directory-part-3.html and
 			http://www.lazywinadmin.com/2013/10/powershell-get-domainuser.html
 #>
-
+	
 	
 	[CmdletBinding()]
 	PARAM (
 		[parameter(mandatory)]
-		[validateset('User', 'Computer', 'Contact', 'Group')] 
+		#[validateset('User','Computer', 'Contact', 'Group', 'PublicFolder', 'OrganizationalUnit')]
+		[validateset('Group')]
 		[String]$ObjectCategory,
-		[String]$Identity = "Admin"
+		[parameter(mandatory)]
+		[String]$Identity,
+		[String]$SearchRoot,
+		[Int]$SizeLimit = 100
+		
 	)
-	
-	$searcher = [adsisearcher]"(&(objectcategory=$ObjectCategory)(|(name=*$Identity*)(objectguid=*$Identity*)(distinguishedname=*$Identity*)(objectsid=*$Identity*)))"
-	
-	FOREACH ($obj in ($searcher.FindAll()))
+	BEGIN { }
+	PROCESS
 	{
-		Write-Verbose -Message "$($obj.properties.name -as [string])"
-		New-Object -TypeName PSObject -Property @{
-			Name = $obj.properties.name -as [string]
-			SamAccountName = $obj.properties.samaccountname -as [string]
-			DistinguishedName = $obj.properties.distinguishedname -as [string]
-			ObjectSID = $obj.properties.objectsid
-			ObjectGUID = $obj.properties.objectguid
-			ObjectCategory = $obj.properties.objectcategory -as [string]
-			ObjectClass = $obj.properties.objectclass -as [string]
-			Description = $obj.properties.description -as [string]
-			GroupType = $obj.properties.grouptype -as [string]
-			WhenCreated = $obj.properties.whencreated
-			WhenChanged = $obj.properties.whenchanged
-			adspath = $obj.properties.adspath
-			usncreated = $obj.properties.usncreated
-			admincount = $obj.properties.admincount -as [int]
-			systemflags = $obj.properties.systemflags
+		
+		$Searcher = [adsisearcher]"(&(objectcategory=$ObjectCategory)(|(name=*$Identity*)(objectguid=*$Identity*)(distinguishedname=*$Identity*)(objectsid=*$Identity*)))"
+		
+		IF ($SearchRoot) { $Searcher.SearchRoot = $SearchRoot }
+		IF ($SizeLimit) { $Searcher.SizeLimit = $SizeLimit }
+		
+		FOREACH ($obj in ($searcher.FindAll()))
+		{
+			Write-Verbose -Message "$($obj.properties.name -as [string])"
+			New-Object -TypeName PSObject -Property @{
+				Name = $obj.properties.name -as [string]
+				SamAccountName = $obj.properties.samaccountname -as [string]
+				DistinguishedName = $obj.properties.distinguishedname -as [string]
+				ObjectSID = $obj.properties.objectsid
+				ObjectGUID = $obj.properties.objectguid
+				ObjectCategory = $obj.properties.objectcategory -as [string]
+				ObjectClass = $obj.properties.objectclass -as [string]
+				Description = $obj.properties.description -as [string]
+				GroupType = $obj.properties.grouptype -as [string]
+				WhenCreated = $obj.properties.whencreated
+				WhenChanged = $obj.properties.whenchanged
+				adspath = $obj.properties.adspath
+				usncreated = $obj.properties.usncreated
+				admincount = $obj.properties.admincount -as [int]
+				systemflags = $obj.properties.systemflags
+			}
 		}
 	}
+	END { }
 }
 
-Get-ADSIObject -ObjectType 'Computer' -Identity 'test'
+Get-ADSIObject -ObjectCategory Group -Identity 'admin'
