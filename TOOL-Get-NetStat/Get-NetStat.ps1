4ba2ac5a2299ebe5e6e1dc0a77f8a30e76a29368
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2014-08-18 20:42:10 -0400

    Upload initial version of Get-Netstat

---
+++ TOOL-Get-NetStat/Get-NetStat.ps1
@@ -0,0 +1,46 @@
+﻿function Get-NetStat
+{
+<#
+.SYNOPSIS
+	This function will get the output of netstat -n and parse the output
+.DESCRIPTION
+	This function will get the output of netstat -n and parse the output
+.LINK
+	http://www.lazywinadmin.com/2014/08/powershell-parse-this-netstatexe.html
+.NOTES
+	Francois-Xavier Cat
+	www.lazywinadmin.com
+	@LazyWinAdm
+#>
+	PROCESS
+	{
+		# Get the output of netstat
+		$data = netstat -n
+		
+		# Keep only the line with the data (we remove the first lines)
+		$data = $data[4..$data.count]
+		
+		# Each line need to be splitted and get rid of unnecessary spaces
+		foreach ($line in $data)
+		{
+			# Get rid of the first whitespaces, at the beginning of the line
+			$line = $line -replace '^\s+', ''
+			
+			# Split each property on whitespaces block
+			$line = $line -split '\s+'
+			
+			# Define the properties
+			$properties = @{
+				Protocole = $line[0]
+				LocalAddressIP = ($line[1] -split ":")[0]
+				LocalAddressPort = ($line[1] -split ":")[1]
+				ForeignAddressIP = ($line[2] -split ":")[0]
+				ForeignAddressPort = ($line[2] -split ":")[1]
+				State = $line[3]
+			}
+			
+			# Output the current line
+			New-Object -TypeName PSObject -Property $properties
+		}
+	}
+}
