78bfd6a335719dbef4d84cd9543147fa1557c549
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2014-10-05 20:12:14 -0400

    Add Get-ADDirectReport

---
+++ AD-USER-Get-ADIndirectReport/Get-ADDirectReport.ps1
@@ -0,0 +1,114 @@
+﻿function Get-ADDirectReports
+{
+	<#
+	.SYNOPSIS
+		This function retrieve the directreports property from the IdentitySpecified.
+		Optionally you can specify the Recurse parameter to find all the indirect
+		users reporting to the specify account (Identity).
+	
+	.DESCRIPTION
+		This function retrieve the directreports property from the IdentitySpecified.
+		Optionally you can specify the Recurse parameter to find all the indirect
+		users reporting to the specify account (Identity).
+	
+	.NOTES
+		Francois-Xavier Cat
+		www.lazywinadmin.com
+		@lazywinadm
+	
+		VERSION HISTORY
+		1.0 2014/10/05 Initial Version
+	
+	.PARAMETER Identity
+		Specify the account to inspect
+	
+	.PARAMETER Recurse
+		Specify that you want to retrieve all the indirect users under the account
+	
+	.EXAMPLE
+		Get-ADDirectReports -Identity Test_director
+	
+Name                SamAccountName      Mail                Manager
+----                --------------      ----                -------
+test_managerB       test_managerB       test_managerB@la... test_director
+test_managerA       test_managerA       test_managerA@la... test_director
+		
+	.EXAMPLE
+		Get-ADDirectReports -Identity Test_director -Recurse
+	
+Name                SamAccountName      Mail                Manager
+----                --------------      ----                -------
+test_managerB       test_managerB       test_managerB@la... test_director
+test_userB1         test_userB1         test_userB1@lazy... test_managerB
+test_userB2         test_userB2         test_userB2@lazy... test_managerB
+test_managerA       test_managerA       test_managerA@la... test_director
+test_userA2         test_userA2         test_userA2@lazy... test_managerA
+test_userA1         test_userA1         test_userA1@lazy... test_managerA
+	
+	#>
+	[CmdletBinding()]
+	PARAM (
+		[Parameter(Mandatory)]
+		[String[]]$Identity,
+		[Switch]$Recurse
+	)
+	BEGIN
+	{
+		TRY
+		{
+			IF (-not (Get-Module -Name ActiveDirectory)) { Import-Module -Name ActiveDirectory -ErrorAction 'Stop' -Verbose:$false }
+		}
+		CATCH
+		{
+			Write-Verbose -Message "[BEGIN] Something wrong happened"
+			Write-Verbose -Message $Error[0].Exception.Message
+		}
+	}
+	PROCESS
+	{
+		foreach ($Account in $Identity)
+		{
+			TRY
+			{
+				IF ($PSBoundParameters['Recurse'])
+				{
+					# Get the DirectReports
+					Write-Verbose -Message "[PROCESS] Account: $Account (Recursive)"
+					Get-Aduser -identity $Account -Properties directreports |
+					ForEach-Object -Process {
+						$_.directreports | ForEach-Object -Process {
+							# Output the current object with the properties Name, SamAccountName, Mail and Manager
+							Get-ADUser -Identity $PSItem -Properties mail, manager | Select-Object -Property Name, SamAccountName, Mail, @{ Name = "Manager"; Expression = { (Get-Aduser -identity $psitem.manager).samaccountname } }
+							# Gather DirectReports under the current object and so on...
+							Get-ADDirectReports -Identity $PSItem -Recurse
+						}
+					}
+				}#IF($PSBoundParameters['Recurse'])
+				IF (-not ($PSBoundParameters['Recurse']))
+				{
+					Write-Verbose -Message "[PROCESS] Account: $Account"
+					# Get the DirectReports
+					Get-Aduser -identity $Account -Properties directreports | Select-Object -ExpandProperty directReports |
+					Get-ADUser -Properties mail, manager | Select-Object -Property Name, SamAccountName, Mail, @{ Name = "Manager"; Expression = { (Get-Aduser -identity $psitem.manager).samaccountname } }
+				}#IF (-not($PSBoundParameters['Recurse']))
+			}#TRY
+			CATCH
+			{
+				Write-Verbose -Message "[PROCESS] Something wrong happened"
+				Write-Verbose -Message $Error[0].Exception.Message
+			}
+		}
+	}
+	END
+	{
+		#Remove-Module -Name ActiveDirectory
+	}
+}
+
+<#
+# Find all direct user reporting to Test_director
+Get-ADDirectReports -Identity Test_director
+
+# Find all Indirect user reporting to Test_director
+Get-ADDirectReports -Identity Test_director -Recurse
+#>
