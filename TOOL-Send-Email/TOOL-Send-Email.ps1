693fe3b645ff91af581bc158427a14ddcbebe6ce
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-02-04 23:05:47 -0500

    Updating Send-Email function

--- TOOL-Send-Email/TOOL-Send-Email.ps1
+++ TOOL-Send-Email/TOOL-Send-Email.ps1
@@ -20,52 +20,120 @@
 		www.lazywinadmin.com
 		@lazywinadm
 	
+		VERSION HISTORY
+		1.0 2014/12/25 	Initial Version
+		1.1 2015/02/04 	Adding some error handling and clean up the code a bit
+						Add Encoding, CC, BCC, BodyAsHTML
+	
+		TODO
+		-Add more Help/Example
+		-Add Support for classic Get-Credential
+	
 	#>
 	
 	[CmdletBinding()]
 	PARAM (
-		[Parameter()]
+		[Parameter(Mandatory = $true)]
 		[Alias('To')]
 		[String]$EmailTo,
-	
-		[String]$Subject,
 		
-		[String]$Body,
-	
-		[Parameter()]
-		[Alias('From')]
 		[Parameter(Mandatory = $true)]
+		[Alias('From')]
 		[String]$EmailFrom,
-	
+		
+		[String]$EmailCC,
+		
+		[String]$EmailBCC,
+		
+		[String]$Subject = "Email from PowerShell",
+		
+		[String]$Body = "Hello World",
+		
+		[Switch]$BodyIsHTML = $false,
+		
+		[ValidateSet("Default","ASCII","Unicode","UTF7","UTF8","UTF32")]
+		[System.Text.Encoding]$Encoding = "Default",
+		
 		[String]$Attachment,
 		
+		[Parameter(ParameterSetName = "Credential", Mandatory = $true)]
 		[String]$Username,
 		
+		[Parameter(ParameterSetName = "Credential", Mandatory = $true)]
 		[String]$Password,
-	
+		
 		[Parameter(Mandatory = $true)]
 		[ValidateScript({
 			# Verify the host is reachable
-			Test-Connection -ComputerName $_ -Count 1 -Quiet
-		})]
+			Test-Connection -ComputerName $_ -Count 1 -Quiet})]
 		[string]$SMTPServer,
-		[ValidateRange(1,65535)]
-		[int]$Port = 587
-	)
 	
-	$SMTPServer = $server
-	$SMTPMessage = New-Object System.Net.Mail.MailMessage($EmailFrom, $EmailTo, $Subject, $Body)
-	if ($PSBoundParameters['attachment'])
+		[ValidateRange(1, 65535)]
+		[int]$Port,
+	
+		[Switch]$EnableSSL
+	)#PARAM
+	
+	PROCESS
 	{
-		$SMTPattachment = New-Object System.Net.Mail.Attachment($attachment)
-		$SMTPMessage.Attachments.Add($STMPattachment)
-	}
-	$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, $port)
-	$SMTPClient.EnableSsl = $true
-	$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($username.Split("@")[0], $Password);
-	#$SMTPClient.Credentials = $SendgridCred
-	$SMTPClient.Send($SMTPMessage)
-	Remove-Variable -Name SMTPClient
-	Remove-Variable -Name Password
-	
+		TRY
+		{
+			# Create Mail Message Object
+			$SMTPMessage = New-Object System.Net.Mail.MailMessage
+			$SMTPMessage.From = $EmailFrom
+			$SMTPMessage.To = $EmailTo
+			$SMTPMessage.Body = $Body
+			$SMTPMessage.Subject = $Subject
+			$SMTPMessage.CC = $EmailCC
+			$SMTPMessage.Bcc = $EmailBCC
+			$SMTPMessage.IsBodyHtml = $BodyIsHtml
+			$SMTPMessage.BodyEncoding = $Encoding
+			$SMTPMessage.SubjectEncoding = $Encoding
+			
+			# Attachement Parameter
+			IF ($PSBoundParameters['attachment'])
+			{
+				$SMTPattachment = New-Object -TypeName System.Net.Mail.Attachment($attachment)
+				$SMTPMessage.Attachments.Add($STMPattachment)
+			}
+			
+			# Create SMTP Client Object
+			$SMTPClient = New-Object Net.Mail.SmtpClient
+			$SMTPClient.Host = $SmtpServer
+			$SMTPClient.Port = $Port
+			
+			# SSL Parameter
+			IF ($PSBoundParameters['EnableSSL'])
+			{
+				$SMTPClient.EnableSsl = $true
+			}
+			
+			# Credential Paramenter
+			IF (($PSBoundParameters['Username']) -and ($PSBoundParameters['Password']))
+			{
+				# Create Credential Object
+				$Credentials = New-Object -TypeName System.Net.NetworkCredential
+				$Credentials.UserName = $username.Split("@")[0]
+				$Credentials.Password = $Password
+				
+				# Add the credentials object to the SMTPClient obj
+				$SMTPClient.Credentials = $Credentials
+			}
+			
+			# Send the Email
+			$SMTPClient.Send($SMTPMessage)
+			
+		}#TRY
+		CATCH
+		{
+			Write-Warning -message "[PROCESS] Something wrong happened"
+			Write-Warning -Message $Error[0].Exception.Message
+		}
+	}#Process
+	END
+	{
+		# Remove Variables
+		Remove-Variable -Name SMTPClient
+		Remove-Variable -Name Password
+	}#END
 } #End Function Send-EMail
