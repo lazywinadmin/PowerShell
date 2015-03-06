4a32256b78c0a0a01d8de1612fe7564f0e68ba05
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-03-06 16:49:13 -0500

    Update RemoteDesktop Functions

---
+++ TOOL-Disable-RemoteDesktop/Disable-RemoteDesktop.ps1
@@ -0,0 +1,42 @@
+﻿function Disable-RemoteDesktop
+{
+  <#
+  .SYNOPSIS
+    Function remotely disable RDP
+  .DESCRIPTION
+    Function remotely disable RDP
+  .NOTES
+    Francois-Xavier Cat
+    @lazywinadm
+    www.lazywinadmin.com
+
+    # MSDN/TechNet doc
+    https://msdn.microsoft.com/en-us/library/aa383644%28v=vs.85%29.aspx
+  #>
+  
+PARAM(
+  [String[]]$ComputerName
+  )
+  FOREACH ($Computer in $ComputerName)
+  {
+    TRY{
+      IF (Test-Connection -Computer $Computer -count 1 -quiet)
+      {
+        $Splatting = @{
+          ComputerName = $Computer
+          NameSpace = rootcimv2TerminalServices
+        }
+        # Enable Remote Desktop
+        (Get-WmiObject -Class Win32_TerminalServiceSetting @Splatting).SetAllowTsConnections(0,0) | Out-Null
+        
+        # Disable requirement that user must be authenticated
+        #(Get-WmiObject -Class Win32_TSGeneralSetting @Splatting -Filter TerminalName='RDP-tcp').SetUserAuthenticationRequired(0)  Out-Null
+      }
+    }
+    CATCH{
+      Write-Warning -Message Something wrong happened
+      Write-Warning -MEssage $Error[0].Exception.Message
+    }
+  }#FOREACH
+  
+}#Function
---
+++ TOOL-Enable-RemoteDesktop/Enable-RemoteDesktop.ps1
@@ -0,0 +1,39 @@
+﻿function Enable-RemoteDesktop
+{
+  <#
+  .SYNOPSIS
+    Function remotely enable RDP
+  .DESCRIPTION
+    Function remotely enable RDP
+  .NOTES
+    Francois-Xavier Cat
+    @lazywinadm
+    www.lazywinadmin.com
+  #>
+  
+PARAM(
+  [String[]]$ComputerName
+  )
+  FOREACH ($Computer in $ComputerName)
+  {
+    TRY{
+      IF (Test-Connection -Computer $Computer -count 1 -quiet)
+      {
+        $Splatting = @{
+          ComputerName = $Computer
+          NameSpace = rootcimv2TerminalServices
+        }
+        # Enable Remote Desktop
+        (Get-WmiObject -Class Win32_TerminalServiceSetting @Splatting).SetAllowTsConnections(1,1) | Out-Null
+        
+        # Disable requirement that user must be authenticated
+        #(Get-WmiObject -Class Win32_TSGeneralSetting @Splatting -Filter TerminalName='RDP-tcp').SetUserAuthenticationRequired(0)  Out-Null
+      }
+    }
+    CATCH{
+      Write-Warning -Message Something wrong happened
+      Write-Warning -MEssage $Error[0].Exception.Message
+    }
+  }#FOREACH
+  
+}#Function
