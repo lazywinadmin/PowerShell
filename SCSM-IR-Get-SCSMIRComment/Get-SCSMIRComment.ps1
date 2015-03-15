4fb6427472d4770c281b40329350adccf775500e
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-03-14 20:42:18 -0400

    Add Get-SCSMIRComment

---
+++ SCSM-IR-Get-SCSMIRComment/Get-SCSMIRComment.ps1
@@ -0,0 +1,61 @@
+﻿function Get-SCSMIRComment
+{
+<#
+	.SYNOPSIS
+		Function to retrieve all the comment of a Incident Request
+	
+	.DESCRIPTION
+		Function to retrieve all the comment of a Incident Request
+	
+	.PARAMETER Incident
+		Specifies the Incident Request Object.
+	
+	.EXAMPLE
+		PS C:\> Get-SCSMIRComment -Incident (get-scsmincident -ID 'IR55444')
+	
+	.NOTES
+		Francois-Xavier Cat
+		www.lazywinadmin.com
+		@lazywinadm
+#>
+	[CmdletBinding()]
+	PARAM
+	(
+		[System.WorkItem.Incident[]]$Incident
+	)
+	PROCESS
+	{
+		FOREACH ($IR in $Incident)
+		{
+			TRY
+			{
+				# Retrieve Comments
+				$FilteredIncidents = $IR.AppliesToTroubleTicket | Where-Object {
+					$_.ClassName -eq "System.WorkItem.TroubleTicket.UserCommentLog" -OR $_.ClassName -eq "System.WorkItem.TroubleTicket.AnalystCommentLog"
+				}
+				
+				IF ($FilteredIncidents.count -gt 0)
+				{
+					FOREACH ($Comment in $FilteredIncidents)
+					{
+						$Properties = @{
+							IncidentID = $IR.ID
+							EnteredDate = $Comment.EnteredDate
+							EnteredBy = $Comment.EnteredBy
+							Comment = $Comment.Comment
+							ClassName = $Comment.ClassName
+							IsPrivate = $Comment.IsPrivate
+						}
+						
+						New-Object -TypeName PSObject -Property $Properties
+					} # FOREACH
+				} #IF Incident found
+			}
+			CATCH
+			{
+				$Error[0]	
+			}
+		} #FOREACH ($IR in $Incident)
+		
+	} #Process
+} #Function
