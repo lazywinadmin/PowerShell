ccb7fd490af489273b92591a7e65fa38d38f602a
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-05-15 20:30:31 -0400

    Add Get-NewPassword

---
+++ TOOL-Get-NewPassword/Get-NewPassword.ps1
@@ -0,0 +1,61 @@
+﻿function Get-NewPassword
+{
+<#
+	.SYNOPSIS
+		Function to Generate a new password.
+	
+	.DESCRIPTION
+		Function to Generate a new password.
+		By default it will generate a 12 characters length password, you can change this using the parameter Length.
+		I excluded the following characters: ",',.,/,<,>,`,O,0
+		You can add exclusion by checking the following ASCII Table http://www.asciitable.com/
+	
+	.PARAMETER Length
+		Specifies the length of the password
+	
+	.EXAMPLE
+		PS C:\> Get-NewPassword -Length 30
+	
+		=E)(71&:f\W6:VRGE(,t1x6sZi-346
+	
+	.NOTES
+		See ASCII Table http://www.asciitable.com/
+		Code based on a blog post of https://mjolinor.wordpress.com/2014/01/31/random-password-generator/
+#>
+	PARAM (
+		[int]$Length = 12
+	)
+	BEGIN
+	{
+		# Create Char Codes 
+		$PasswordCharCodes = { 33..126 }.invoke()
+		
+		# Exclude ",',.,/,<,>,`,O,0
+		# See http://www.asciitable.com/ for mapping
+		34, 39, 46, 47, 60, 62, 96, 48, 79 | ForEach-Object { [void]$PasswordCharCodes.Remove($_) }
+		$PasswordChars = [char[]]$PasswordCharCodes
+	}
+	PROCESS
+	{
+		DO
+		{
+			# Generate a Password of the length requested
+			$NewPassWord = $(foreach ($i in 1..$length) { Get-Random -InputObject $PassWordChars }) -join ''
+		}#Do
+		UNTIL (
+		
+			# Make sure it contains an Upercase and Lowercase letter, a number and another special character
+			($NewPassword -cmatch '[A-Z]') -and
+			($NewPassWord -cmatch '[a-z]') -and
+			($NewPassWord -imatch '[0-9]') -and
+			($NewPassWord -imatch '[^A-Z0-9]')
+		)#Until
+		
+		#Output new password
+		$NewPassword
+	}
+	END
+	{
+		Remove-Variable -Name NewPassWord -ErrorAction 'SilentlyContinue'
+	} #END
+} # Function
