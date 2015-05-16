1da91b5ae61a0c2438b3ec4538a54cf6324ca44c
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-05-15 20:44:08 -0400

    Update Get-NewPassword
    Add a condition if the password is equal or less than 4 characters, it
    will generate a random password. If higher or equal to 5, it will make
    sure there are Upper and Lower Case letter, a Number and a special
    character.

--- TOOL-Get-NewPassword/Get-NewPassword.ps1
+++ TOOL-Get-NewPassword/Get-NewPassword.ps1
@@ -10,6 +10,9 @@
 		I excluded the following characters: ",',.,/,1,<,>,`,O,0,l,|
 		You can add exclusion by checking the following ASCII Table http://www.asciitable.com/
 	
+		If the length requested is less or equal to 4, it will generate a random password.
+		If the length requested is greater than 4, it will make sure the password contains an Upper and Lower case letter, a Number and a special character
+	
 	.PARAMETER Length
 		Specifies the length of the password
 	
@@ -17,17 +20,22 @@
 		PS C:\> Get-NewPassword -Length 30
 		
 		=E)(71&:f\W6:VRGE(,t1x6sZi-346
+
+    .EXAMPLE
+        PS C:\> Get-NewPassword 3
+        
+        !}R
 	
 	.NOTES
 		See ASCII Table http://www.asciitable.com/
 		Code based on a blog post of https://mjolinor.wordpress.com/2014/01/31/random-password-generator/
 #>
 	[CmdletBinding()]
-	param
+	PARAM
 	(
 		[ValidateNotNull()]
 		[int]$Length = 12
-	)
+	)#PARAM
 	
 	BEGIN
 	{
@@ -38,28 +46,35 @@
 		# See http://www.asciitable.com/ for mapping
 		34, 39, 46, 47, 49, 60, 62, 96, 48, 79, 108, 124 | ForEach-Object { [void]$PasswordCharCodes.Remove($_) }
 		$PasswordChars = [char[]]$PasswordCharCodes
-	}
+	}#BEGIN
 	PROCESS
 	{
-		DO
+		IF ($Length -gt 4)
 		{
-			# Generate a Password of the length requested
+			
+			DO
+			{
+				# Generate a Password of the length requested
+				$NewPassWord = $(foreach ($i in 1..$length) { Get-Random -InputObject $PassWordChars }) -join ''
+			}#Do
+			UNTIL (
+			# Make sure it contains an Upercase and Lowercase letter, a number and another special character
+			($NewPassword -cmatch '[A-Z]') -and
+			($NewPassWord -cmatch '[a-z]') -and
+			($NewPassWord -imatch '[0-9]') -and
+			($NewPassWord -imatch '[^A-Z0-9]')
+			)#Until
+		}#IF
+		ELSE
+		{
 			$NewPassWord = $(foreach ($i in 1..$length) { Get-Random -InputObject $PassWordChars }) -join ''
-		}#Do
-		UNTIL (
+		}#ELSE
 		
-		# Make sure it contains an Upercase and Lowercase letter, a number and another special character
-		($NewPassword -cmatch '[A-Z]') -and
-		($NewPassWord -cmatch '[a-z]') -and
-		($NewPassWord -imatch '[0-9]') -and
-		($NewPassWord -imatch '[^A-Z0-9]')
-		)#Until
-		
-		#Output new password
+		# Output a new password
 		$NewPassword
-	}
+	} #PROCESS
 	END
 	{
 		Remove-Variable -Name NewPassWord -ErrorAction 'SilentlyContinue'
 	} #END
-} # Function
+} #Function
