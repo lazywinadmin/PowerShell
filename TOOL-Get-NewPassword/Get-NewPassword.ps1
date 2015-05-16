21ce2c211139ea44e856681ea7e97d62a1984b0d
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-05-15 20:36:38 -0400

    Update Get-NewPassword
    Add exception to not return 1 or l or | to avoid confusion
    Add ValidateNotNull on $length

--- TOOL-Get-NewPassword/Get-NewPassword.ps1
+++ TOOL-Get-NewPassword/Get-NewPassword.ps1
@@ -7,7 +7,7 @@
 	.DESCRIPTION
 		Function to Generate a new password.
 		By default it will generate a 12 characters length password, you can change this using the parameter Length.
-		I excluded the following characters: ",',.,/,<,>,`,O,0
+		I excluded the following characters: ",',.,/,1,<,>,`,O,0,l,|
 		You can add exclusion by checking the following ASCII Table http://www.asciitable.com/
 	
 	.PARAMETER Length
@@ -15,24 +15,28 @@
 	
 	.EXAMPLE
 		PS C:\> Get-NewPassword -Length 30
-	
+		
 		=E)(71&:f\W6:VRGE(,t1x6sZi-346
 	
 	.NOTES
 		See ASCII Table http://www.asciitable.com/
 		Code based on a blog post of https://mjolinor.wordpress.com/2014/01/31/random-password-generator/
 #>
-	PARAM (
+	[CmdletBinding()]
+	param
+	(
+		[ValidateNotNull()]
 		[int]$Length = 12
 	)
+	
 	BEGIN
 	{
 		# Create Char Codes 
 		$PasswordCharCodes = { 33..126 }.invoke()
 		
-		# Exclude ",',.,/,<,>,`,O,0
+		# Exclude ",',.,/,1,<,>,`,O,0,l,|
 		# See http://www.asciitable.com/ for mapping
-		34, 39, 46, 47, 60, 62, 96, 48, 79 | ForEach-Object { [void]$PasswordCharCodes.Remove($_) }
+		34, 39, 46, 47, 49, 60, 62, 96, 48, 79, 108, 124 | ForEach-Object { [void]$PasswordCharCodes.Remove($_) }
 		$PasswordChars = [char[]]$PasswordCharCodes
 	}
 	PROCESS
@@ -44,11 +48,11 @@
 		}#Do
 		UNTIL (
 		
-			# Make sure it contains an Upercase and Lowercase letter, a number and another special character
-			($NewPassword -cmatch '[A-Z]') -and
-			($NewPassWord -cmatch '[a-z]') -and
-			($NewPassWord -imatch '[0-9]') -and
-			($NewPassWord -imatch '[^A-Z0-9]')
+		# Make sure it contains an Upercase and Lowercase letter, a number and another special character
+		($NewPassword -cmatch '[A-Z]') -and
+		($NewPassWord -cmatch '[a-z]') -and
+		($NewPassWord -imatch '[0-9]') -and
+		($NewPassWord -imatch '[^A-Z0-9]')
 		)#Until
 		
 		#Output new password
