45884d4a9662a9f8bb00dc8117fc4d8831e39f68
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-05-20 17:00:49 -0400

    Add Remove-StringDiacritics

---
+++ TOOL-Remove-StringDiacritics/Remove-StringDiacritics.ps1
@@ -0,0 +1,55 @@
+﻿function Remove-StringDiacritics
+{
+<#
+	.SYNOPSIS
+		This function will remove the diacritics characters from a string
+	
+	.DESCRIPTION
+		This function will remove the diacritics characters from a string
+	
+	.PARAMETER String
+		Specifies the String on which the diacritics need to be removed
+	
+	.PARAMETER NormalizationForm
+		Specifies the normalization form to use
+		https://msdn.microsoft.com/en-us/library/system.text.normalizationform(v=vs.110).aspx
+	
+	.EXAMPLE
+		PS C:\> Remove-StringDiacritics "L'été de Raphaël"
+		
+		L'ete de Raphael
+	
+	.NOTES
+		Francois-Xavier Cat
+		@lazywinadm
+		www.lazywinadmin.com
+#>
+	
+	param
+	(
+		[ValidateNotNullOrEmpty()]
+		[Alias('Text')]
+		[System.String]$String,
+		[System.Text.NormalizationForm]$NormalizationForm = "FormD"
+	)
+	
+	BEGIN
+	{
+		$Normalized = $String.Normalize($NormalizationForm)
+		$NewString = New-Object -TypeName System.Text.StringBuilder
+		
+	}
+	PROCESS
+	{
+		$normalized.ToCharArray() | ForEach-Object -Process {
+			if ([Globalization.CharUnicodeInfo]::GetUnicodeCategory($psitem) -ne [Globalization.UnicodeCategory]::NonSpacingMark)
+			{
+				[void]$NewString.Append($psitem)
+			}
+		}
+	}
+	END
+	{
+		Write-Output $($NewString -as [string])
+	}
+}
