0bd46af0a636c363f4ce79d16b8f18bd0dbc3338
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-05-20 21:52:23 -0400

    Add Get-StringCharCount

---
+++ TOOL-Get-StringCharCount/Get-StringCharCount.ps1
@@ -0,0 +1,19 @@
+﻿Function Get-StringCharCount
+{
+	<#
+	.SYNOPSIS
+		This function will count the number of characters in a string
+	.DESCRIPTION
+		This function will count the number of characters in a string
+	.EXAMPLE
+		PS C:\> Get-StringCharCount -String "Hello World"
+	
+		11
+	.NOTES
+		Francois-Xavier Cat
+		@lazywinadm
+		www.lazywinadmin.com	
+	#>
+	PARAM ([String]$String)
+	($String -as [Char[]]).count
+}
