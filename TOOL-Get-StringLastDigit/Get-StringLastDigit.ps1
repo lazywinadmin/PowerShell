61cdd12d6fc4429170a200ccaffdd0349f7502df
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-05-22 11:46:36 -0400

    Add Get-StringLastDigit

---
+++ TOOL-Get-StringLastDigit/Get-StringLastDigit.ps1
@@ -0,0 +1,37 @@
+﻿function Get-StringLastDigit
+{
+<#
+    .SYNOPSIS
+        Get the last digit of a string
+    .DESCRIPTION
+        Get the last digit of a string using Regular Expression
+    .PARAMETER String
+        Specifies the String to check
+    .EXAMPLE
+        PS C:\> Get-StringLastDigit -String "Francois-Xavier.cat5"
+
+        5
+    .EXAMPLE
+        PS C:\> Get-StringLastDigit -String "Francois-Xavier.cat"
+
+        <no output>
+    .EXAMPLE
+        PS C:\> Get-StringLastDigit -String "Francois-Xavier.cat" -Verbose
+
+        <no output>
+        VERBOSE: The following string does not finish by a digit: Francois-Xavier.cat
+    .NOTES
+        Francois-Xavier Cat
+        @lazywinadm
+        www.lazywinadmin.com
+#>
+[CmdletBinding()]
+PARAM($String)
+    #Check if finish by Digit
+    if ($String -match "^.*\d$")
+    {
+        # Output the last digit
+        $String.Substring(($String.ToCharArray().count)-1)
+    }
+    else {Write-Verbose -Message "The following string does not finish by a digit: $String"}
+}
