c6a6f74248aa09a53575032f5c2fb28e8eb520d6
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-06-17 15:26:58 -0400

    Add Get-HashTableNotEmptyOrNullValue
    Function to find keys with values

---
+++ TOOL-Get-HashTableNotEmptyOrNullValue/Get-HashTableNotEmptyOrNullValue.ps1
@@ -0,0 +1,26 @@
+﻿Function Get-HashTableNotEmptyOrNullValue
+{
+<#
+.SYNOPSIS
+    This function will get the values that are not empty or Null in a hashtable object
+.DESCRIPTION
+    This function will get the values that are not empty or Null in a hashtable object
+.PARAMETER Hashtable
+    Specifies the hashtable that will be showed
+.EXAMPLE
+    Get-HashTableNotEmptyOrNullValue -HashTable $SplattingVariable
+.NOTES
+    Francois-Xavier Cat
+    @lazywinadm
+    www.lazywinadmin.com
+#>
+    PARAM([System.Collections.Hashtable]$HashTable)
+
+    $HashTable.GetEnumerator().name |
+        ForEach-Object -Process {
+            if($HashTable[$_] -ne "" -or $HashTable[$_] -ne $null)
+            {
+                Write-Output $_
+            }
+        }
+}
