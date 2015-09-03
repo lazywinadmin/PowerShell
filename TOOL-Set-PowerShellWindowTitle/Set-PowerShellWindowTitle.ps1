0d4cba816cf404c1407700dc033b996186be2fb6
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-09-03 00:33:10 -0400

    Add Set-PowerShellWindowTitle

---
+++ TOOL-Set-PowerShellWindowTitle/Set-PowerShellWindowTitle.ps1
@@ -0,0 +1,24 @@
+﻿function Set-PowerShellWindowTitle
+{
+<#
+	.SYNOPSIS
+		Function to set the title of the PowerShell Window
+	
+	.DESCRIPTION
+		Function to set the title of the PowerShell Window
+	
+	.PARAMETER Title
+		Specifies the Title of the PowerShell Window
+	
+	.EXAMPLE
+		PS C:\> Set-PowerShellWindowTitle -Title LazyWinAdmin.com
+	
+	.NOTES
+		Francois-Xavier Cat
+		www.lazywinadmin.com
+		@lazywinadm
+#>
+	PARAM($Title)
+	$Host.UI.RawUI.WindowTitle = $Title	
+}
+
