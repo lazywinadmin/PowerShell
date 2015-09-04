2c3b3ff5f36834fa6da39ffe78313c02ac42ea3f
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-09-04 18:18:06 -0400

    Update Add-SCSMWorkItemRAReviewer

--- SCSM-Add-SCSMWorkItemRAReviewer/Add-SCSMWorkItemRAReviewer.ps1
+++ SCSM-Add-SCSMWorkItemRAReviewer/Add-SCSMWorkItemRAReviewer.ps1
@@ -16,11 +16,11 @@
 	.PARAMETER MustVote
 		Specifies if the Vote is required. Default is False.
 	
-	.PARAMETER WorkItem
-		Specifies the WorkItem object
+	.PARAMETER WorkItemID
+		Specifies the WorkItem ID of the Review Activity
 	
 	.EXAMPLE
-		PS C:\> Add-SCSMWorkItemRAReviewer -UserName 'francois-xavier' -veto $true
+		PS C:\> Add-SCSMWorkItemRAReviewer -UserName 'francois-xavier' -veto $true -WorkItemID '2aa822b0-b144-3acf-bee3-9a11714c5de0'
 	
 	.NOTES
 		Francois-Xavier Cat
@@ -38,7 +38,7 @@
 		
 		[Boolean]$mustvote = $false,
 		
-		$WorkItem
+		$WorkItemID
 	)
 	
 	BEGIN { Import-Module -Name SMLets -ErrorAction Stop }
@@ -46,7 +46,7 @@
 	{
 		# Retrieve the Active Directory User Class
 		$ADUserClassID = '10a7f898-e672-ccf3-8881-360bfb6a8f9a'
-		$ADUserClassObject = Get-ScsmClass -id $id_adUserClass
+		$ADUserClassObject = Get-ScsmClass -Id $ADUserClassID
 		
 		$ScsmUser = Get-ScsmObject -class $ADUserClassObject -filter "Username -eq $UserName"
 		
@@ -61,6 +61,8 @@
 			$ReviewerArgs = @{ ReviewerID = "{0}"; Mustvote = $mustvote; Veto = $veto }
 			
 			$Reviewer = new-scsmobject -class $class_ReviewerClass -propertyhashtable $ReviewerArgs -nocommit
+			
+			$WorkItem = Get-SCSMObject -Class (Get-SCSMClass -Name System.WorkItem.Activity.ReviewActivity$) -filter "ID -eq '$WorkItemID'"
 			
 			$reviewerStep1 = New-SCSMRelationshipObject -nocommit -Relationship $RelationShipClass_HasReviewer -Source $WorkItem -Target $Reviewer
 			$reviewerStep2 = New-SCSMRelationshipObject -nocommit -Relationship $RelationShipClass_ReviewerIsUser -Source $Reviewer -Target $ScsmUser
