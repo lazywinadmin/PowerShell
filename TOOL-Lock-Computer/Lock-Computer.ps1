0afc4e488ddb3114329e8fd25b17ded8b476d227
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-11-19 14:22:03 -0500

    Add Lock-Computer

---
+++ TOOL-Lock-Computer/Lock-Computer.ps1
@@ -0,0 +1,17 @@
+Function Lock-Computer
+{
+	<#
+		.DESCRIPTION
+		Function to Lock your computer
+		.SYNOPSIS
+		Function to Lock your computer
+	#>
+	
+$signature = @"
+[DllImport("user32.dll", SetLastError = true)]
+public static extern bool LockWorkStation();
+"@
+
+	$LockComputer = Add-Type -memberDefinition $signature -name "Win32LockWorkStation" -namespace Win32Functions -passthru
+	$LockComputer::LockWorkStation() | Out-Null
+}
