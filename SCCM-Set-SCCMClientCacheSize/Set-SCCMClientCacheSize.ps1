7635d208872d467e196519deef51517696327fbb
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-11-19 14:22:47 -0500

    Add Set-SCCMClientCacheSize

---
+++ SCCM-Set-SCCMClientCacheSize/Set-SCCMClientCacheSize.ps1
@@ -0,0 +1,8 @@
+﻿function Set-SCCMClientCacheSize
+{
+	PARAM($ComputerName)
+	$Cache = Get-WmiObject -Namespace ‘ROOT\CCM\SoftMgmtAgent’ -Class CacheConfig -ComputerName $ComputerName
+	$Cache.Size = ‘10240’
+	$Cache.Put()
+	Get-Service -ComputerName $ComputerName -Name 'ccmexec'| Restart-Service -Name CcmExec
+}
