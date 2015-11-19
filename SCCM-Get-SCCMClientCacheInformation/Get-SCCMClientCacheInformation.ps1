dc7011b4e8feabc04d82707fda15d6073d190cf4
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-11-19 15:41:51 -0500

    Add  Get-SCCMClientCacheInformation

---
+++ SCCM-Get-SCCMClientCacheInformation/Get-SCCMClientCacheInformation.ps1
@@ -0,0 +1,57 @@
+﻿function Get-SCCMClientCacheInformation
+{
+	<#
+		.SYNOPSYS
+			Function to get the cache size on a SCCM Client
+		.DESCRIPTION
+			Function to get the cache size on a SCCM Client
+		.PARAMETER ComputerName
+			Specifies the name of the client
+		.PARAMETER Credential
+			Specifies the credential to use against the remote machine
+			Only work with the WMI query for now, not the service restart
+		.EXAMPLE
+			Get-SCCMClientCacheInformation -ComputerName Client01
+			
+			This will get the client cache size on the computer Client01
+	#>
+	PARAM(
+		[string[]]$ComputerName=".",
+		
+		[Alias('RunAs')]
+		[System.Management.Automation.Credential()]
+		$Credential = [System.Management.Automation.PSCredential]::Empty
+	)
+
+	FOREACH ($Computer in $ComputerName)
+	{
+		Write-Verbose -message "[PROCESS] ComputerName: $Computer"
+		
+		# Define Parameters
+		$SplattingWMI = @{
+			NameSpace = "ROOT\CCM\SoftMgmtAgent"
+			Class = "CacheConfig"
+		}
+
+		IF ($PSBoundParameters['ComputerName'])
+		{
+			$SplattingWMI.ComputerName = $Computer
+		}
+		IF ($PSBoundParameters['Credential'])
+		{
+			$SplattingWMI.Credential = $Credential
+		}
+		
+		TRY
+		{
+			# Get the Client information
+			Get-WmiObject @SplattingWMI
+			
+		}
+		CATCH
+		{
+			Write-Warning -message "[PROCESS] Something Wrong happened with $Computer"
+			$Error[0].execption.message
+		}
+	}
+}
