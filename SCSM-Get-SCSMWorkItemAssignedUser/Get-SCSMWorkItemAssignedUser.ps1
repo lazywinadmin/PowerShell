2f0bf57c9a99b406920226e0b1d4d7158a0726b3
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2016-01-29 10:45:26 -0500

    Update SCSM Assigned and Affected Functions

--- SCSM-Get-SCSMWorkItemAssignedUser/Get-SCSMWorkItemAssignedUser.ps1
+++ SCSM-Get-SCSMWorkItemAssignedUser/Get-SCSMWorkItemAssignedUser.ps1
@@ -1,26 +1,26 @@
-﻿function Get-SCSMWorkItemAffectedUser
+﻿function Get-SCSMWorkItemAssignedUser
 {
 <#
 	.SYNOPSIS
-		Function to retrieve the Affected User of a Work Item
+		Function to retrieve the Assigned User of a Work Item
 	
 	.DESCRIPTION
-		Function to retrieve the Affected User of a Work Item
+		Function to retrieve the Assigned User of a Work Item
 	
 	.PARAMETER SMObject
-		Specifies the SMObject(s) on which the affected need to be retrieve.
+		Specifies the SMObject(s) on which the Assigned need to be retrieve.
 	
 	.PARAMETER Guid
-		Specifies the GUID of the SMObject on which the affected need to be retrieve.
+		Specifies the GUID of the SMObject on which the Assigned need to be retrieve.
 	
 	.EXAMPLE
-		Get-SCSMWorkItemAffectedUser -SMObject $SR,IR
+		Get-SCSMWorkItemAssignedUser -SMObject $SR,IR
 	
 	.EXAMPLE
-		$SR,IR | Get-SCSMWorkItemAffectedUser
+		$SR,IR | Get-SCSMWorkItemAssignedUser
 	
 	.EXAMPLE
-		Get-SCSMWorkItemAffectedUser -GUID 5bd5e783-c8a1-0217-9e19-f82823ef4f87
+		Get-SCSMWorkItemAssignedUser -GUID 5bd5e783-c8a1-0217-9e19-f82823ef4f87
 	
 	.NOTES
 		Francois-Xavier Cat
@@ -45,9 +45,8 @@
 	{
 		Import-Module -Name SMLets -ErrorAction Stop
 		
-		# AffectedUser RelationshipClass
-		$RelationshipClass_AffectedUser = 'dff9be66-38b0-b6d6-6144-a412a3ebd4ce'
-		$RelationshipClass_AffectedUser_Object = Get-SCSMRelationshipClass -id $RelationshipClass_AffectedUser
+		# AssignedUser RelationshipClass
+		$RelationshipClass_AssignedUser_Object = Get-SCSMRelationshipClass -Name System.WorkItemAssignedToUser$
 	}
 	PROCESS
 	{
@@ -57,7 +56,7 @@
 			{
 				$SMObject = Get-SCSMObject -id $item
 				Write-Verbose -Message "[PROCESS] Working on $($Item.Name)"
-				Get-ScsmRelatedObject -SMObject $SMObject -Relationship $RelationshipClass_AffectedUser_Object |
+				Get-ScsmRelatedObject -SMObject $SMObject -Relationship $RelationshipClass_AssignedUser_Object |
 				Select-Object -Property @{ Label = "WorkItemName"; Expression = { $SMObject.Name } },
 							  @{ Label = "WorkItemGUID"; Expression = { $SMObject.get_id() } }, *
 			}
@@ -68,7 +67,7 @@
 			foreach ($Item in $SMObject)
 			{
 				Write-Verbose -Message "[PROCESS] Working on $($Item.Name)"
-				Get-ScsmRelatedObject -SMObject $Item -Relationship $RelationshipClass_AffectedUser_Object |
+				Get-ScsmRelatedObject -SMObject $Item -Relationship $RelationshipClass_AssignedUser_Object |
 				Select-Object -Property @{ Label = "WorkItemName"; Expression = { $Item.Name } },
 							  @{ Label = "WorkItemGUID"; Expression = { $Item.get_id() } }, *
 			}
