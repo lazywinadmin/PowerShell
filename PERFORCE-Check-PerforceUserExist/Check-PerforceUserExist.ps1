c0e6cae44991da3025fcb4aab4e2c76c5ea1dffc
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2016-02-14 22:14:19 -0500

    Add some Perforce functions

---
+++ PERFORCE-Check-PerforceUserExist/Check-PerforceUserExist.ps1
@@ -0,0 +1,19 @@
+﻿Function Check-PerforceUserExist
+{
+	PARAM ($UserName)
+	PROCESS
+	{
+		$CheckAccount = p4 users $UserName
+		if ($CheckAccount -like "*accessed*")
+		{
+			$CheckAccount = $CheckAccount -split '\s'
+			
+			$Properties = @{
+				"UserName" = $CheckAccount[0]
+				"Email" = $CheckAccount[1] -replace "<|>", ""
+				"PerforceAccount" = $CheckAccount[2] -replace "\(|\)", ""
+			}
+			New-Object -TypeName PSObject -Property $Properties
+		} #IF
+	}
+}
---
+++ PERFORCE-Connect-PerforceServer/Connect-PerforceServer.ps1
@@ -0,0 +1,51 @@
+﻿function Connect-PerforceServer
+{
+<#
+.SYNOPSIS
+    Connect to a Perforce Server
+.DESCRIPTION
+    Connect to a Perforce Server
+#>
+	[CmdletBinding()]
+	PARAM (
+		[Parameter(Mandatory = $True)]
+		$Server,
+		
+		[Parameter(Mandatory = $True)]
+		$Port,
+		
+		[Parameter(Mandatory = $True)]
+		$UserName,
+		
+		[Parameter(Mandatory = $True)]
+		$Password
+	)
+	BEGIN
+	{
+		#Check if p4.exe is present
+	}
+	PROCESS
+	{
+		TRY
+		{
+			IF (Test-Connection -ComputerName $Server -Count 1 -Quiet -ErrorAction SilentlyContinue)
+			{
+				# Set the Environment Variables
+				$env:p4port = "$($Server):$($Port)"
+				$env:p4user = $UserName
+				
+				# Connect to p4 as Admin
+				#  The environment variables previously set will be used by p4
+				$Password | p4 login
+			}
+			ELSE
+			{
+				Write-Warning -Message "[PROCESS] Can't ping to $Server on port: $port"
+			}
+		}
+		CATCH
+		{
+			Write-Warning -Message "[PROCESS] Issue while connecting to perforce server: $Server on port: $port"
+		}
+	}
+}
