e1425311cdb3423823ab52fefe493263093f321b
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2016-04-09 19:07:57 -0400

    Add Base64 functions

---
+++ TOOL-ConvertFrom-Base64/ConvertFrom-Base64.ps1
@@ -0,0 +1,36 @@
+﻿function ConvertFrom-Base64
+{
+	<#
+	.SYNOPSIS
+		Converts the specified string, which encodes binary data as base-64 digits, to an equivalent 8-bit unsigned integer array.
+	
+	.DESCRIPTION
+		Converts the specified string, which encodes binary data as base-64 digits, to an equivalent 8-bit unsigned integer array.
+	
+	.PARAMETER String
+		Specifies the String to Convert
+	
+	.NOTES
+		Francois-Xavier Cat
+		@lazywinadm
+		www.lazywinadmin.com
+		github.com/lazywinadmin
+#>
+	[CmdletBinding()]
+	PARAM (
+		[parameter(Mandatory = $true, ValueFromPipeline)]
+		[String]$String
+	)
+	TRY
+	{
+		Write-Verbose -Message "[ConvertFrom-Base64] Converting String"
+		[System.Text.Encoding]::Default.GetString(
+		[System.Convert]::FromBase64String($String)
+		)
+	}
+	CATCH
+	{
+		Write-Error -Message "[ConvertFrom-Base64] Something wrong happened"
+		$Error[0].Exception.Message
+	}
+}
---
+++ TOOL-ConvertTo-Base64/ConvertTo-Base64.ps1
@@ -0,0 +1,30 @@
+﻿function ConvertTo-Base64
+{
+<#
+	.SYNOPSIS
+		Function to convert an image to Base64
+	
+	.DESCRIPTION
+		Function to convert an image to Base64
+	
+	.PARAMETER Path
+		Specifies the path of the file
+	
+	.NOTES
+		Francois-Xavier Cat
+		@lazywinadm
+		www.lazywinadmin.com
+		github.com/lazywinadmin
+#>
+	
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true)]
+		[ValidateScript({ Test-Path -Path $_ })]
+		[String]$Path
+	)
+	Write-Verbose -Message "[ConvertTo-Base64] Converting image to Base64 $Path"
+	[System.convert]::ToBase64String((Get-Content -Path $path -Encoding Byte))
+}
