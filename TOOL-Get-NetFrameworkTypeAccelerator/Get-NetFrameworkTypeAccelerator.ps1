003bab37b799038d2d306a664c7d060d4b082582
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2016-07-06 00:13:31 -0400

    Add Get-NetFrameworkTypeAccelerator

---
+++ TOOL-Get-NetFrameworkTypeAccelerator/Get-NetFrameworkTypeAccelerator.ps1
@@ -0,0 +1,24 @@
+﻿function Get-NetFrameworkTypeAccelerator
+{
+<#
+.SYNOPSIS 
+	Function to retrieve the list of Type Accelerator available
+.EXAMPLE
+	Get-NetFrameworkTypeAccelerator
+	
+	Return the list of Type Accelerator available on your system
+.EXAMPLE
+	Get-Accelerator
+	
+	Return the list of Type Accelerator available on your system
+	This is a function alias created by [Alias()]
+.NOTES
+	Francois-Xavier Cat
+	lazywinadmin.com
+	@lazywinadm
+	github.com/lazywinadmin
+#>
+	[Alias('Get-Acceletrator')]
+	PARAM ()
+	[System.Management.Automation.PSObject].Assembly.GetType("System.Management.Automation.TypeAccelerators")::get
+}
