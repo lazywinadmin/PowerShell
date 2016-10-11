fea20f812e5a23891d47f08458637ac59b203890
François-Xavier Cat <fxcat@lazywinadmin.com>
2016-10-10 20:11:01 -0400

    Update Remove-StringLatinCharacter

--- TOOL-Remove-StringLatinCharacter/Remove-StringLatinCharacter.ps1
+++ TOOL-Remove-StringLatinCharacter/Remove-StringLatinCharacter.ps1
@@ -1,7 +1,44 @@
 ﻿function Remove-StringLatinCharacters
 {
-	#http://www.lazywinadmin.com/2015/05/powershell-remove-diacritics-accents.html
-	#Method 2 (From Marcin Krzanowicz)
-	PARAM ([string]$String)
-	[Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding("Cyrillic").GetBytes($String))
+<#
+.SYNOPSIS
+    Function to remove diacritics from a string
+.EXAMPLE
+    Remove-StringLatinCharacters -String "L'été de Raphaël"
+
+    L'ete de Raphael
+.EXAMPLE
+    Foreach ($file in (Get-ChildItem c:\test\*.txt))
+    {
+        # Get the content of the current file and remove the diacritics
+        $NewContent = Get-content $file | Remove-StringLatinCharacters
+    
+        # Overwrite the current file with the new content
+        $NewContent | Set-Content $file
+    }
+
+    Remove diacritics from multiple files
+
+.NOTES
+    Francois-Xavier Cat
+    @lazywinadm
+    github.com/lazywinadmin
+
+    BLOG ARTICLE
+        http://www.lazywinadmin.com/2015/05/powershell-remove-diacritics-accents.html
+	
+    VERSION HISTORY
+        1.0.0.0 | Francois-Xavier Cat
+            Initial version Based on Marcin Krzanowic code
+        1.0.0.1 | Francois-Xavier Cat
+            Added support for ValueFromPipeline
+#>
+	PARAM (
+		[Parameter(ValueFromPipeline=$true)]
+		[System.String]$String
+		)
+	PROCESS
+	{
+		[Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding("Cyrillic").GetBytes($String))
+	}
 }
