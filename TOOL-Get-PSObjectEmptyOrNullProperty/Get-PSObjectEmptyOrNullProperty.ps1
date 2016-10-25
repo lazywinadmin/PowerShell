bc2e6fb5b879fb507d03c0b5bc4a22236c9e29c7
François-Xavier Cat <lazywinadmin@users.noreply.github.com>
2016-10-24 23:23:40 -0400

    Update Get-PSObjectEmptyOrNullProperty.ps1

--- TOOL-Get-PSObjectEmptyOrNullProperty/Get-PSObjectEmptyOrNullProperty.ps1
+++ TOOL-Get-PSObjectEmptyOrNullProperty/Get-PSObjectEmptyOrNullProperty.ps1
@@ -1,22 +1,49 @@
-﻿function Get-PSObjectEmptyOrNullProperty
+function Get-PSObjectEmptyOrNullProperty
 {
 <#
-	.SYNOPSIS
-		Function to Get all the empty or null properties with empty value in a PowerShell Object
+.SYNOPSIS
+	Function to Get all the empty or null properties with empty value in a PowerShell Object
 	
-	.DESCRIPTION
-		Function to Get all the empty or null properties with empty value in a PowerShell Object
+.DESCRIPTION
+	Function to Get all the empty or null properties with empty value in a PowerShell Object
 	
-	.PARAMETER PSObject
-		Specifies the PowerShell Object
+.PARAMETER PSObject
+	Specifies the PowerShell Object
 	
-	.EXAMPLE
-		PS C:\> Get-PSObjectEmptyOrNullProperty -PSObject $UserInfo
+.EXAMPLE
+	PS C:\> Get-PSObjectEmptyOrNullProperty -PSObject $UserInfo
 	
-	.NOTES
-		Francois-Xavier Cat	
-		www.lazywinadmin.com
-		@lazywinadm
+.EXAMPLE
+
+    # Create a PowerShell Object with some properties
+    $o=''|select FirstName,LastName,nullable
+    $o.firstname='Nom'
+    $o.lastname=''
+    $o.nullable=$null
+	
+    # Look for empty or null properties
+    Get-PSObjectEmptyOrNullProperty -PSObject $o
+
+    MemberType      : NoteProperty
+    IsSettable      : True
+    IsGettable      : True
+    Value           : 
+    TypeNameOfValue : System.String
+    Name            : LastName
+    IsInstance      : True
+
+    MemberType      : NoteProperty
+    IsSettable      : True
+    IsGettable      : True
+    Value           : 
+    TypeNameOfValue : System.Object
+    Name            : nullable
+    IsInstance      : True
+
+.NOTES
+	Francois-Xavier Cat	
+	www.lazywinadmin.com
+	@lazywinadm
 #>
 	PARAM (
 		$PSObject)
