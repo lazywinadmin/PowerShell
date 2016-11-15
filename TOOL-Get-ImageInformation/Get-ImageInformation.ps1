6a7610c861d3ce5231c1d64540019f1d346dbe9a
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2016-11-14 20:35:19 -0500

    Add Get-ImageInformation

---
+++ TOOL-Get-ImageInformation/Get-ImageInformation.ps1
@@ -0,0 +1,33 @@
+﻿function Get-ImageInformation
+{
+<#
+.SYNOPSIS
+	function to retrieve Image file information
+
+.DESCRIPTION
+	function to retrieve Image file information
+
+.PARAMETER FilePath
+	Specify one or multiple image file path(s).
+
+.EXAMPLE
+	PS C:\> Get-ImageInformation -FilePath c:\temp\image.png
+
+.NOTES
+	Francois-Xavier Cat
+	lazywinadmin.com
+	@lazywinadm
+	github.com/lazywinadmin
+#>
+	PARAM (
+		[System.String[]]$FilePath
+	)
+	Foreach ($Image in $FilePath)
+	{
+		# Load Assembly
+		Add-type -AssemblyName System.Drawing
+		
+		# Retrieve information
+		New-Object -TypeName System.Drawing.Bitmap -ArgumentList $Image
+	}
+}
