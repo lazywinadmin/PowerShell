38e75e25e86c091f3b8df1a1e165ed20e530c12c
François-Xavier Cat <fxcat@lazywinadmin.com>
2017-07-05 22:19:56 -0400

    Add verbose messages and update error handling

--- AD-SITE-Get-ADSiteInventory/Get-ADSiteInventory.ps1
+++ AD-SITE-Get-ADSiteInventory/Get-ADSiteInventory.ps1
@@ -12,50 +12,58 @@
 	.EXAMPLE
 		Get-ADSiteInventory | Export-Csv -Path .\ADSiteInventory.csv
 
+		This will save all the site inventory to csv file
+		
 	.OUTPUTS
 		PSObject
 
 	.NOTES
 		AUTHOR	: Francois-Xavier Cat
 		DATE	: 2014/02/02
-		
-		HISTORY	:
-	
-			1.0		2014/02/02	Initial Version
-			
+		VERSION HISTORY	:
+			1.0 | 2014/02/02 | Francois-Xavier Cat
+				Initial Version
+			1.1 | 2014/02/02 | Francois-Xavier Cat
+				Update some verbose messages
 	
 #>
 	[CmdletBinding()]
     PARAM()
-    BEGIN {Write-Verbose -Message "[BEGIN] Starting Script..."}
     PROCESS
     {
 		TRY{
+			# Get Script name
+			$ScriptName = (Get-Variable -name MyInvocation -Scope 0 -ValueOnly).Mycommand
+
 	        # Domain and Sites Information
+			Write-Verbose -message "[$ScriptName][PROCESS] Retrieve current Forest"
 	        $Forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+			Write-Verbose -message "[$ScriptName][PROCESS] Retrieve current Forest sites"
 	        $SiteInfo = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().Sites
 
 	        # Forest Context
+			Write-Verbose -message "[$ScriptName][PROCESS] Create forest context"
 	        $ForestType = [System.DirectoryServices.ActiveDirectory.DirectoryContexttype]"forest"
 	        $ForestContext = New-Object -TypeName System.DirectoryServices.ActiveDirectory.DirectoryContext -ArgumentList $ForestType,$Forest
             
             # Distinguished Name of the Configuration Partition
+			Write-Verbose -message "[$ScriptName][PROCESS] Retrieve RootDSE Configuration Naming Context"
             $Configuration = ([ADSI]"LDAP://RootDSE").configurationNamingContext
 
             # Get the Subnet Container
+			Write-Verbose -message "[$ScriptName][PROCESS] Get the Subnet Container"
             $SubnetsContainer = [ADSI]"LDAP://CN=Subnets,CN=Sites,$Configuration"
-
 
 	        FOREACH ($item in $SiteInfo){
 				
-				Write-Verbose -Message "[PROCESS] SITE: $($item.name)"
+				Write-Verbose -Message "[$ScriptName][PROCESS] SITE: $($item.name)"
 				
 				# Get the Site Links
-				Write-Verbose -Message "[PROCESS] SITE: $($item.name) - Getting Site Links"
+				Write-Verbose -Message "[$ScriptName][PROCESS] SITE: $($item.name) - Getting Site Links"
 	            $LinksInfo = ([System.DirectoryServices.ActiveDirectory.ActiveDirectorySite]::FindByName($ForestContext,$($item.name))).SiteLinks
 				
 				# Create PowerShell Object and Output
-				Write-Verbose -Message "[PROCESS] SITE: $($item.name) - Preparing Output"
+				Write-Verbose -Message "[$ScriptName][PROCESS] SITE: $($item.name) - Preparing Output"
 
 	            New-Object -TypeName PSObject -Property @{
 	                Name= $item.Name
@@ -86,13 +94,13 @@
 		}#TRY
 		CATCH
 		{
-			Write-Warning -Message "[PROCESS] Something Wrong Happened"
-			Write-Warning -Message $Error[0]
+			# Return the last error
+			$PSCmdlet.ThrowTerminatingError($_)
 		}#CATCH
     }#PROCESS
     END
 	{
-		Write-Verbose -Message "[END] Script Completed!"
+		Write-Verbose -Message "[$ScriptName][END] Script Completed!"
 	}#END
 }#get-ADSiteServicesInfo
 
