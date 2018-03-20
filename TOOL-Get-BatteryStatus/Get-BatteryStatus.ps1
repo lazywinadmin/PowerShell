4a890054ab1bff8e209a2f711eabd1072b4c6a6e
François-Xavier Cat <fxcat@lazywinadmin.com>
2018-03-19 21:57:42 -0700

    Add Get-BatteryStatus

---
+++ TOOL-Get-BatteryStatus/Get-BatteryStatus.ps1
@@ -0,0 +1,23 @@
+function Get-BatteryStatus {
+    <#
+    .SYNOPSIS
+    Retrieve battery information
+    
+    .DESCRIPTION
+    Retrieve battery information
+    
+    .EXAMPLE
+    Get-Battery
+    
+    .NOTES
+        http://www.powershellmagazine.com/2012/10/18/pstip-get-system-power-information/
+    #>
+    PARAM()
+    try{
+        Add-Type -Assembly System.Windows.Forms
+        [System.Windows.Forms.SystemInformation]::PowerStatus
+    }catch
+    {
+        $PSCmdlet.ThrowTerminatingError($_)
+    }
+}
