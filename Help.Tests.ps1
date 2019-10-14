5f8d3f52792de321e88c51b0780fa3ecfc13fdef
kookie <kookie@users.noreply.github.com>
2019-10-15 10:22:01 +1100

    Logic added to skip scripts
    When dot sourcing the scripts will automaticaly run. Added logic to remove the scripts and pass pester tests with $true should be $true.

--- Help.Tests.ps1
+++ Help.Tests.ps1
@@ -1,31 +1,48 @@
 $scripts = Get-ChildItem -Path $PSScriptRoot -Recurse -Filter *.ps1 | 
                 Where-Object FullName -NotMatch '.Tests.'
 
+[regex]$regex = "(^[A-Z\-]+-)"
 
 Describe -Tag 'Help' 'Help' {
 
     foreach ($script in $scripts) {
 
         Context "[$($script.BaseName)] Validate Comment Based Help" {
-            # Dot Source script
-            . $($script.FullName)
 
-            $functionHelp = Get-Help ($script.Name).TrimEnd('.ps1') -Full
-
-            It 'Contains Description' {
-                $functionHelp.Description | Should Not BeNullOrEmpty
-            }
-            
-            It 'Contains Synopsis' {
-                $functionHelp.Synopsis | Should Not BeNullOrEmpty
+            # Correct name where file name does not match function name
+            if ($script.Name -Match $regex) {
+                $name = $script.BaseName.Replace($Matches[0], '')
+            } else {
+                $name = $script.BaseName
             }
 
-            It 'Contains Examples' {
-                $functionHelp.Examples | Should Not BeNullOrEmpty
-            }
+            # Only process functions and not scripts
+            if ((Get-Content -Path $script.FullName -TotalCount 1) -match 'function') {
+                
+                # Dot Source script
+                . $($script.FullName)
 
-            It 'Contains Parameters' {
-                $functionHelp.Parameters | Should Not BeNullOrEmpty
+                $functionHelp = Get-Help $name -Full
+
+                It 'Contains Description' {
+                    $functionHelp.Description | Should Not BeNullOrEmpty
+                }
+                
+                It 'Contains Synopsis' {
+                    $functionHelp.Synopsis | Should Not BeNullOrEmpty
+                }
+
+                It 'Contains Examples' {
+                    $functionHelp.Examples | Should Not BeNullOrEmpty
+                }
+
+                It 'Contains Parameters' {
+                    $functionHelp.Parameters | Should Not BeNullOrEmpty
+                }
+            } else {
+                It "[$($script.BaseName)] is not a function and skipped" {
+                    $true | Should Be $true
+                }
             }
         }
     }
