92f8cb73bafb8785c1d0cadce0089adfa6197099
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-26 15:41:17 -0800

    Reformat script

--- SCSM-IR-Get-SCSMIRComment/Get-SCSMIRComment.ps1
+++ SCSM-IR-Get-SCSMIRComment/Get-SCSMIRComment.ps1
@@ -1,6 +1,5 @@
-﻿function Get-SCSMIRComment
-{
-<#
+﻿function Get-SCSMIRComment {
+    <#
     .SYNOPSIS
         Function to retrieve all the comment of a Incident Request
 
@@ -23,39 +22,32 @@
     (
         [System.WorkItem.Incident[]]$Incident
     )
-    PROCESS
-    {
-        FOREACH ($IR in $Incident)
-        {
-            TRY
-            {
+    PROCESS {
+        FOREACH ($IR in $Incident) {
+            TRY {
                 # Retrieve Comments
                 $FilteredIncidents = $IR.AppliesToTroubleTicket | Where-Object {
                     $_.ClassName -eq "System.WorkItem.TroubleTicket.UserCommentLog" -OR $_.ClassName -eq "System.WorkItem.TroubleTicket.AnalystCommentLog"
                 }
 
-                IF ($FilteredIncidents.count -gt 0)
-                {
-                    FOREACH ($Comment in $FilteredIncidents)
-                    {
+                IF ($FilteredIncidents.count -gt 0) {
+                    FOREACH ($Comment in $FilteredIncidents) {
                         $Properties = @{
-                            IncidentID = $IR.ID
+                            IncidentID  = $IR.ID
                             EnteredDate = $Comment.EnteredDate
-                            EnteredBy = $Comment.EnteredBy
-                            Comment = $Comment.Comment
-                            ClassName = $Comment.ClassName
-                            IsPrivate = $Comment.IsPrivate
+                            EnteredBy   = $Comment.EnteredBy
+                            Comment     = $Comment.Comment
+                            ClassName   = $Comment.ClassName
+                            IsPrivate   = $Comment.IsPrivate
                         }
 
                         New-Object -TypeName PSObject -Property $Properties
                     } # FOREACH
                 } #IF Incident found
             }
-            CATCH
-            {
-                $Error[0]
+            CATCH {
+                $PSCmdlet.ThrowTerminatingError($_)
             }
         } #FOREACH ($IR in $Incident)
-
     } #Process
 } #Function
