30bd9cc453e932b7d2f834afff83c655f67049be
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-27 21:54:48 -0800

    fix command based help

--- TOOL-Lock-Computer/Lock-Computer.ps1
+++ TOOL-Lock-Computer/Lock-Computer.ps1
@@ -1,17 +1,22 @@
 Function Lock-Computer
 {
-	<#
-		.DESCRIPTION
-		Function to Lock your computer
-		.SYNOPSIS
-		Function to Lock your computer
-	#>
+<#
+.DESCRIPTION
+    Function to Lock your computer
+.SYNOPSIS
+    Function to Lock your computer.
+    This is using the win32 user32.dll library.
+.EXAMPLE
+    Lock-Computer
+
+    This will lock the current computer
+#>
 
 $signature = @"
 [DllImport("user32.dll", SetLastError = true)]
 public static extern bool LockWorkStation();
 "@
 
-	$LockComputer = Add-Type -memberDefinition $signature -name "Win32LockWorkStation" -namespace Win32Functions -passthru
-	$LockComputer::LockWorkStation() | Out-Null
+    $LockComputer = Add-Type -memberDefinition $signature -name "Win32LockWorkStation" -namespace Win32Functions -passthru
+    $LockComputer::LockWorkStation() | Out-Null
 }
