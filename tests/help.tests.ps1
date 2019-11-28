9b87160bd1021ff0d3d6d72774bb0916af670032
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-27 23:05:43 -0800

    Update Pester tests

--- tests/help.tests.ps1
+++ tests/help.tests.ps1
@@ -1,31 +1,34 @@
 $scripts = Get-ChildItem -Path (Split-Path $PSScriptRoot -parent) -Recurse -Filter *.ps1 |
-    Where-Object -FilterScript {
-        $_.FullName -NotMatch '.Tests.' -and
-        $_.Fullname -notmatch [regex]::Escape('_Profiles') -and
-        $_.Fullname -notmatch [regex]::Escape('_Template')
-    } | Sort-Object
+Where-Object -FilterScript {
+    $_.FullName -NotMatch '.Tests.' -and
+    $_.Fullname -notmatch [regex]::Escape('_Profiles') -and
+    $_.Fullname -notmatch [regex]::Escape('_Template')
+} | Sort-Object
 
 [regex]$regex = "(^[A-Z\-]+-)"
 
-Describe -Tag 'Help' 'Help' {
-
+Describe 'Comment based help' -Tag @('Help') {
     foreach ($script in $scripts) {
+        $FileContent = Get-Content -Path $script.FullName -TotalCount 1
 
         Context "[$($script.BaseName)] Validate Comment Based Help" {
 
             # Correct name where file name does not match function name
             if ($script.Name -Match $regex) {
                 $name = $script.BaseName.Replace($Matches[0], '')
-            } elseif ($script.Name -match 'O365-') {
+            }
+            elseif ($script.Name -match 'O365-') {
                 $name = $script.BaseName.Replace($Matches[0], '')
-            } elseif ($script.Name -match 'Function_Template.ps1') {
+            }
+            elseif ($script.Name -match 'Function_Template.ps1') {
                 $name = 'Get-Something'
-            } else {
+            }
+            else {
                 $name = $script.BaseName
             }
 
             # Only process functions and not scripts
-            if ((Get-Content -Path $script.FullName -TotalCount 1) -match 'function') {
+            if ($FileContent -match 'function') {
 
                 # Dot Source script
                 . $($script.FullName)
@@ -46,16 +49,23 @@ Describe -Tag 'Help' 'Help' {
                     $functionHelp.Examples | Should Not BeNullOrEmpty
                 }
 
-                if($functionAST.ParamBlock)
-                {
+                if ($functionAST.ParamBlock) {
                     It 'Contains Parameters' {
                         $functionHelp.Parameters | Should Not BeNullOrEmpty
                     }
                 }
-            } else {
-                It "[$($script.BaseName)] is not a function" {
+            }
+            else {
+                It "[$($script.BaseName)] is not a unique function" {
                 } -Skip
             }
+        }
+    }
+}
+Describe 'func' -Tag @('func') {
+    foreach ($myscript in $scripts) {
+        it "[$($myscript.BaseName)] `$Error[*]" {
+            "$($myscript.FullName)" | Should -Not -FileContentMatch ([regex]::Escape('$error['))
         }
     }
 }
