be97c9a402174f68f32192d2597e2e09c130fa44
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-29 14:04:31 -0800

    Update format

--- O365-GROUP-Get-DistributionGroupRecursive/O365-GROUP-Get-DistributionGroupRecursive.ps1
+++ O365-GROUP-Get-DistributionGroupRecursive/O365-GROUP-Get-DistributionGroupRecursive.ps1
@@ -1,7 +1,6 @@
 ﻿
-function Get-DistributionGroupMemberRecursive
-{
-<#
+function Get-DistributionGroupMemberRecursive {
+    <#
 .SYNOPSIS
     This script will list all the members (recursively) of a DistributionGroup
 .EXAMPLE
@@ -13,37 +12,30 @@ function Get-DistributionGroupMemberRecursive
 #>
     [CmdletBinding()]
     PARAM ($Group)
-    BEGIN
-    {
-        TRY
-        {
+    BEGIN {
+        TRY {
             # Retrieve Group information
             Write-Verbose -Message "[BEGIN] Retrieving members of $Group"
             $GroupMembers = Get-DistributionGroupMember -Identity $Group -ErrorAction Stop -ErrorVariable ErrorBeginGetDistribMembers |
             Select-object -Property Name, PrimarySMTPAddress, @{ Label = "Group"; Expression = { $Group } }, RecipientType
 
         }
-        CATCH
-        {
+        CATCH {
             Write-Warning -Message "[BEGIN] Something wrong happened"
             if ($ErrorBeginGetDistribMembers) { Write-Warning -Message "[BEGIN] Issue while retrieving members of $Group" }
             Write-Warning -Message $Error[0].Exception.Message
         }
     }
-    PROCESS
-    {
-        FOREACH ($Member in $GroupMembers)
-        {
-            TRY
-            {
+    PROCESS {
+        FOREACH ($Member in $GroupMembers) {
+            TRY {
                 Write-verbose "[PROCESS] Member: $($member.name)"
 
-                SWITCH ($Member.RecipientType)
-                {
+                SWITCH ($Member.RecipientType) {
                     "MailUniversalDistributionGroup" {
                         # Member's type is Distribution Group, we need to find members of this object
                         Get-DistributionGroupMemberRecursive -Group $($Member.name) |
-                            Select-Object -Property Name, PrimarySMTPAddress, @{ Label = "Group"; Expression = { $($Member.name) } }, RecipientType
+                        Select-Object -Property Name, PrimarySMTPAddress, @{ Label = "Group"; Expression = { $($Member.name) } }, RecipientType
                         Write-Verbose -Message "[PROCESS] $($Member.name)"
                     }
                     "UserMailbox" {
@@ -52,15 +44,13 @@ function Get-DistributionGroupMemberRecursive
                     }
                 }
             }
-            CATCH
-            {
+            CATCH {
                 Write-Warning -Message "[PROCESS] Something wrong happened"
                 Write-Warning -Message $Error[0].Exception.Message
             }
         }
     }
-    END
-    {
+    END {
         Write-Verbose -message "[END] Done"
     }
 }
