82ae9d6035d6a1cfa551b04a0ed5a6332ff0ec8f
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-28 18:20:24 -0800

    Replace tabs

--- TOOL-Test-IsLocalAdministrator/Test-IsLocalAdministrator.ps1
+++ TOOL-Test-IsLocalAdministrator/Test-IsLocalAdministrator.ps1
@@ -2,18 +2,18 @@
 {
 <#
 .SYNOPSIS
-	Function to verify if the current user is a local Administrator on the current system
+    Function to verify if the current user is a local Administrator on the current system
 .DESCRIPTION
-	Function to verify if the current user is a local Administrator on the current system
+    Function to verify if the current user is a local Administrator on the current system
 .EXAMPLE
-	Test-IsLocalAdministrator
+    Test-IsLocalAdministrator
 
-	True
+    True
 .NOTES
-	Francois-Xavier Cat
-	@lazywinadmin
-	lazywinadmin.com
-	github.com/lazywinadmin
+    Francois-Xavier Cat
+    @lazywinadmin
+    lazywinadmin.com
+    github.com/lazywinadmin
 #>
-	([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+    ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 }
