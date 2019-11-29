b5a8689b2484750999cbf7d480e825fd4db29238
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-28 18:17:56 -0800

    Add error handling

--- TOOL-Remove-StringSpecialCharacter/Remove-StringSpecialCharacter.ps1
+++ TOOL-Remove-StringSpecialCharacter/Remove-StringSpecialCharacter.ps1
@@ -50,25 +50,29 @@ function Remove-StringSpecialCharacter
         [String[]]$SpecialCharacterToKeep
     )
     PROCESS {
-        IF ($PSBoundParameters["SpecialCharacterToKeep"]) {
-            $Regex = "[^\p{L}\p{Nd}"
-            Foreach ($Character in $SpecialCharacterToKeep) {
-                IF ($Character -eq "-") {
-                    $Regex += "-"
+        try{
+            IF ($PSBoundParameters["SpecialCharacterToKeep"]) {
+                $Regex = "[^\p{L}\p{Nd}"
+                Foreach ($Character in $SpecialCharacterToKeep) {
+                    IF ($Character -eq "-") {
+                        $Regex += "-"
+                    }
+                    else {
+                        $Regex += [Regex]::Escape($Character)
+                    }
+                    #$Regex += "/$character"
                 }
-                else {
-                    $Regex += [Regex]::Escape($Character)
-                }
-                #$Regex += "/$character"
+
+                $Regex += "]+"
+            } #IF($PSBoundParameters["SpecialCharacterToKeep"])
+            ELSE { $Regex = "[^\p{L}\p{Nd}]+" }
+
+            FOREACH ($Str in $string) {
+                Write-Verbose -Message "Original String: $Str"
+                $Str -replace $regex, ""
             }
-
-            $Regex += "]+"
-        } #IF($PSBoundParameters["SpecialCharacterToKeep"])
-        ELSE { $Regex = "[^\p{L}\p{Nd}]+" }
-
-        FOREACH ($Str in $string) {
-            Write-Verbose -Message "Original String: $Str"
-            $Str -replace $regex, ""
+        }catch{
+            $PSCmdlet.ThrowTerminatingError($_)
         }
     } #PROCESS
 }
