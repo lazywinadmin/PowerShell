53ca7af29c818157789a96dde8f6c4bb22884321
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-28 16:02:35 -0800

    Fix error handling, update syntax format

--- VMWARE-HOST-List_VIB/VMWARE-HOST-List_VIB.ps1
+++ VMWARE-HOST-List_VIB/VMWARE-HOST-List_VIB.ps1
@@ -18,64 +18,52 @@ This Script retrieve the VIB information on all the VMware Host
     lazywinadmin.com
     @lazywinadmin
 #>
-[CmdletBinding(DefaultParameterSetName="All")]
+[CmdletBinding(DefaultParameterSetName = "All")]
 PARAM (
     [parameter(Mandatory = $true)]
+    [pscredential]
     $Credential,
     [parameter(Mandatory = $true)]
     $Vcenter,
-    [Parameter(mandatory=$true,ParameterSetName = "All")]
+    [Parameter(mandatory = $true, ParameterSetName = "All")]
     [Switch]$AllVib,
-    [Parameter(mandatory=$true,ParameterSetName = "VIBName")]
+    [Parameter(mandatory = $true, ParameterSetName = "VIBName")]
     $VibName,
-    [Parameter(mandatory=$true,ParameterSetName = "VIBVendor")]
+    [Parameter(mandatory = $true, ParameterSetName = "VIBVendor")]
     $VibVendor
 )
-BEGIN
-{
-    TRY
-    {
+BEGIN {
+    TRY {
         # Verify VMware Snapin is loaded
-        IF (-not (Get-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction 'SilentlyContinue'))
-        {
+        IF (-not (Get-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction 'SilentlyContinue')) {
             Write-Verbose -Message "BEGIN - Loading Vmware Snapin VMware.VimAutomation.Core..."
             Add-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction Stop -ErrorVariable ErrorBeginAddPssnapin
         }
 
         # Verify VMware Snapin is connected to at least one vcenter
-        IF (-not ($global:DefaultVIServer.count -gt 0))
-        {
+        IF (-not ($global:DefaultVIServer.count -gt 0)) {
             Write-Verbose -Message "BEGIN - Currently not connected to a vCenter..."
             $Vcenter = Read-Host -Prompt "You are not connected to a VMware vCenter, Please enter the FQDN or IP of the vCenter"
 
-            IF ((Read-Host -Prompt "You are currently logged as: $($env:username). Do you want to specify different credential ? (Y/N)") -eq 'Y')
-            {
+            IF ((Read-Host -Prompt "You are currently logged as: $($env:username). Do you want to specify different credential ? (Y/N)") -eq 'Y') {
                 Connect-VIServer -Server $Vcenter -credential (Get-Credential) -ErrorAction Stop -ErrorVariable ErrorBeginConnectViServer
             }
-            ELSE
-            {
+            ELSE {
                 Connect-VIServer -Server $Vcenter -ErrorAction Stop -ErrorVariable ErrorBeginConnectViServer
             }
         }
     }
-    CATCH
-    {
-        Write-Warning -Message "Can't Connect to the Vcenter Server $vcenter"
-        Write-Warning -Message $Error[0].Exception.Message
+    CATCH {
+        $PSCmdlet.ThrowTerminatingError($_)
     }
 }
-PROCESS
-{
-    TRY
-    {
+PROCESS {
+    TRY {
         $VMHosts = Get-VMHost -ErrorAction Stop -ErrorVariable ErrorGetVMhost | Where-Object { $_.ConnectionState -eq "Connected" }
 
-        IF ($PSBoundParameters['AllVib'])
-        {
-            Foreach ($CurrentVMhost in $VMHosts)
-            {
-                TRY
-                {
+        IF ($PSBoundParameters['AllVib']) {
+            Foreach ($CurrentVMhost in $VMHosts) {
+                TRY {
                     # Exposes the ESX CLI functionality of the current host
                     $ESXCLI = Get-EsxCli -VMHost $CurrentVMhost.name
                     # Retrieve Vibs
@@ -83,14 +71,14 @@ PROCESS
                     ForEach-Object {
                         $VIB = $_
                         $Prop = [ordered]@{
-                            'VMhost' = $CurrentVMhost.Name
-                            'ID' = $VIB.ID
-                            'Name' = $VIB.Name
-                            'Vendor' = $VIB.Vendor
-                            'Version' = $VIB.Version
-                            'Status' = $VIB.Status
-                            'ReleaseDate' = $VIB.ReleaseDate
-                            'InstallDate' = $VIB.InstallDate
+                            'VMhost'          = $CurrentVMhost.Name
+                            'ID'              = $VIB.ID
+                            'Name'            = $VIB.Name
+                            'Vendor'          = $VIB.Vendor
+                            'Version'         = $VIB.Version
+                            'Status'          = $VIB.Status
+                            'ReleaseDate'     = $VIB.ReleaseDate
+                            'InstallDate'     = $VIB.InstallDate
                             'AcceptanceLevel' = $VIB.AcceptanceLevel
                         }#$Prop
 
@@ -98,19 +86,15 @@ PROCESS
                         New-Object PSobject -Property $Prop
                     }#FOREACH
                 }#TRY
-                CATCH
-                {
+                CATCH {
                     Write-Warning -Message "Something wrong happened with $($CurrentVMhost.name)"
                     Write-Warning -Message $Error[0].Exception.Message
                 }
             }
         }
-        IF ($PSBoundParameters['VibVendor'])
-        {
-            Foreach ($CurrentVMhost in $VMHosts)
-            {
-                TRY
-                {
+        IF ($PSBoundParameters['VibVendor']) {
+            Foreach ($CurrentVMhost in $VMHosts) {
+                TRY {
                     # Exposes the ESX CLI functionality of the current host
                     $ESXCLI = Get-EsxCli -VMHost $CurrentVMhost.name
                     # Retrieve Vib from vendor $vibvendor
@@ -119,14 +103,14 @@ PROCESS
                     {
                         $VIB = $_
                         $Prop = [ordered]@{
-                            'VMhost' = $CurrentVMhost.Name
-                            'ID' = $VIB.ID
-                            'Name' = $VIB.Name
-                            'Vendor' = $VIB.Vendor
-                            'Version' = $VIB.Version
-                            'Status' = $VIB.Status
-                            'ReleaseDate' = $VIB.ReleaseDate
-                            'InstallDate' = $VIB.InstallDate
+                            'VMhost'          = $CurrentVMhost.Name
+                            'ID'              = $VIB.ID
+                            'Name'            = $VIB.Name
+                            'Vendor'          = $VIB.Vendor
+                            'Version'         = $VIB.Version
+                            'Status'          = $VIB.Status
+                            'ReleaseDate'     = $VIB.ReleaseDate
+                            'InstallDate'     = $VIB.InstallDate
                             'AcceptanceLevel' = $VIB.AcceptanceLevel
                         }#$Prop
 
@@ -134,19 +118,14 @@ PROCESS
                         New-Object PSobject -Property $Prop
                     }#FOREACH
                 }#TRY
-                CATCH
-                {
-                    Write-Warning -Message "Something wrong happened with $($CurrentVMhost.name)"
-                    Write-Warning -Message $Error[0].Exception.Message
+                CATCH {
+                    $PSCmdlet.ThrowTerminatingError($_)
                 }
             }
         }
-        IF ($PSBoundParameters['VibName'])
-        {
-            Foreach ($CurrentVMhost in $VMHosts)
-            {
-                TRY
-                {
+        IF ($PSBoundParameters['VibName']) {
+            Foreach ($CurrentVMhost in $VMHosts) {
+                TRY {
                     # Exposes the ESX CLI functionality of the current host
                     $ESXCLI = Get-EsxCli -VMHost $CurrentVMhost.name
                     # Retrieve Vib with name $vibname
@@ -155,14 +134,14 @@ PROCESS
                     {
                         $VIB = $_
                         $Prop = [ordered]@{
-                            'VMhost' = $CurrentVMhost.Name
-                            'ID' = $VIB.ID
-                            'Name' = $VIB.Name
-                            'Vendor' = $VIB.Vendor
-                            'Version' = $VIB.Version
-                            'Status' = $VIB.Status
-                            'ReleaseDate' = $VIB.ReleaseDate
-                            'InstallDate' = $VIB.InstallDate
+                            'VMhost'          = $CurrentVMhost.Name
+                            'ID'              = $VIB.ID
+                            'Name'            = $VIB.Name
+                            'Vendor'          = $VIB.Vendor
+                            'Version'         = $VIB.Version
+                            'Status'          = $VIB.Status
+                            'ReleaseDate'     = $VIB.ReleaseDate
+                            'InstallDate'     = $VIB.InstallDate
                             'AcceptanceLevel' = $VIB.AcceptanceLevel
                         }#$Prop
 
@@ -170,18 +149,13 @@ PROCESS
                         New-Object PSobject -Property $Prop
                     }#FOREACH
                 }#TRY
-                CATCH
-                {
-                    Write-Warning -Message "Something wrong happened with $($CurrentVMhost.name)"
-                    Write-Warning -Message $Error[0].Exception.Message
+                CATCH {
+                    $PSCmdlet.ThrowTerminatingError($_)
                 }
             }
         }
     }
-    CATCH
-    {
-        Write-Warning -Message "Something wrong happened in the script"
-        IF ($ErrorGetVMhost) { Write-Warning -Message "Couldn't retrieve VMhosts" }
-        Write-Warning -Message $Error[0].Exception.Message
+    CATCH {
+        throw $_
     }
 }
