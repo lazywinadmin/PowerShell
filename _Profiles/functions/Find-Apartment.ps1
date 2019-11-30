78891cbe48dc5fc28990b865c134162d1caaa64a
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-29 23:18:28 -0800

    Update format

--- _Profiles/functions/Find-Apartment.ps1
+++ _Profiles/functions/Find-Apartment.ps1
@@ -1,5 +1,4 @@
-﻿Function Find-Apartment
-{
+Function Find-Apartment {
     <#
     .SYNOPSIS
         Allow you search Appartement in craigslist
@@ -8,33 +7,33 @@
         #http://masterrex.com/?p=64
     #>
     param (
-        [Parameter(Mandatory=$False)]$MinPrice="0",
-        [Parameter(Mandatory=$False)]$MaxPrice="9999",
-        [Parameter(Mandatory=$False)]$MaxPages="1",
-        [Parameter(Mandatory=$False)]$URL = "http://burlington.craigslist.org"
+        [Parameter(Mandatory = $False)]$MinPrice = "0",
+        [Parameter(Mandatory = $False)]$MaxPrice = "9999",
+        [Parameter(Mandatory = $False)]$MaxPages = "1",
+        [Parameter(Mandatory = $False)]$URL = "http://burlington.craigslist.org"
     )
     $AvailableRooms = @()
-    For ($CurrentPage=0;$CurrentPage -le $MaxPages;$CurrentPage++) {
+    For ($CurrentPage = 0; $CurrentPage -le $MaxPages; $CurrentPage++) {
         $WebPage = Invoke-WebRequest "$URL/search/roo?=roo&s=$Start&query=&zoomToPosting=&minAsk=$MinPrice&maxAsk=$MaxPrice&hasPic=1"
         $Results = $WebPage.ParsedHtml.body.innerHTML.Split("`n") | Where-Object { $_ -like "<P class=row*" }
         ForEach ($Item in $Results) {
-            $ItemObject=$ID=$Price=$DatePosted=$Neighborhood=$Link=$Description=$Email=$null
-            $ID = ($Item -replace ".*pid\=`"","") -replace "`".*"
-            $Price = ($Item -replace ".*class=price>","") -replace "</SPAN>.*"
-            $DatePosted = ($Item -replace ".*class=date>","") -replace "</SPAN>.*"
-            $Neighborhood = ($Item -replace ".*\<SMALL\>\(","") -replace "\)\</SMALL>.*"
+            $ItemObject = $ID = $Price = $DatePosted = $Neighborhood = $Link = $Description = $Email = $null
+            $ID = ($Item -replace ".*pid\=`"", "") -replace "`".*"
+            $Price = ($Item -replace ".*class=price>", "") -replace "</SPAN>.*"
+            $DatePosted = ($Item -replace ".*class=date>", "") -replace "</SPAN>.*"
+            $Neighborhood = ($Item -replace ".*\<SMALL\>\(", "") -replace "\)\</SMALL>.*"
             If ($Neighborhood -like "<*") { $Neighborhood = "N/A" }
-            $Link = $URL + ((($Item -replace ".*\<A href\=`"","") -replace "\<.*") -split('">'))[0]
+            $Link = $URL + ((($Item -replace ".*\<A href\=`"", "") -replace "\<.*") -split ('">'))[0]
             $Email = (($(Invoke-WebRequest $Link).ParsedHtml.body.innerHTML.Split("`n") | Where-Object { $_ -like "var displayEmail*" }) -replace "var displayEmail \= `"") -replace "`";"
-            $Description = ((($Item -replace ".*\<A href\=`"","") -replace "\<.*") -split('">'))[1]
+            $Description = ((($Item -replace ".*\<A href\=`"", "") -replace "\<.*") -split ('">'))[1]
             $ItemObject = New-Object -TypeName PSObject -Property @{
-                'ID' = $ID
-                'Price' = $Price
-                'DatePosted' = $DatePosted
+                'ID'           = $ID
+                'Price'        = $Price
+                'DatePosted'   = $DatePosted
                 'Neighborhood' = $Neighborhood
-                'Link' = $Link
-                'Description' = $Description
-                'E-Mail' = $Email
+                'Link'         = $Link
+                'Description'  = $Description
+                'E-Mail'       = $Email
             }
             #$AvailableRooms += $ItemObject
             $ItemObject
@@ -42,3 +41,4 @@
     }
     #Return $AvailableRooms
 }
+
