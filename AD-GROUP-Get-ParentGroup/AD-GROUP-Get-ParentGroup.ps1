c76ae7b5cc84c8f2d86e7ffd38e6227b4cc47a35
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-29 23:30:01 -0800

    Update format

--- AD-GROUP-Get-ParentGroup/AD-GROUP-Get-ParentGroup.ps1
+++ AD-GROUP-Get-ParentGroup/AD-GROUP-Get-ParentGroup.ps1
@@ -1,6 +1,5 @@
-﻿function Get-ParentGroup
-{
-<#
+function Get-ParentGroup {
+    <#
     .SYNOPSIS
         Find all Nested members of a group
     .DESCRIPTION
@@ -26,40 +25,34 @@
         [Parameter(Mandatory = $true)]
         [String[]]$Name
     )
-    BEGIN
-    {
-        TRY{
-            if(-not(Get-Module Activedirectory -ErrorAction Stop)){
+    BEGIN {
+        TRY {
+            if (-not(Get-Module Activedirectory -ErrorAction Stop)) {
                 Write-Verbose -Message "[BEGIN] Loading ActiveDirectory Module"
-                Import-Module ActiveDirectory -ErrorAction Stop}
+                Import-Module ActiveDirectory -ErrorAction Stop
+            }
         }
-        CATCH
-        {
+        CATCH {
             $PSCmdlet.ThrowTerminatingError($_)
         }
     }
-    PROCESS
-    {
-        TRY
-        {
-            FOREACH ($Obj in $Name)
-            {
+    PROCESS {
+        TRY {
+            FOREACH ($Obj in $Name) {
                 # Make an Ambiguous Name Resolution
                 $ADObject = Get-ADObject -LDAPFilter "(|(anr=$obj)(distinguishedname=$obj))" -Properties memberof -ErrorAction Stop
-                IF($ADObject)
-                {
+                IF ($ADObject) {
                     # Show a warning if more than 1 object is found
-                    if ($ADObject.count -gt 1){Write-Warning -Message "More than one object found with the $obj request"}
+                    if ($ADObject.count -gt 1) { Write-Warning -Message "More than one object found with the $obj request" }
 
-                    FOREACH ($Account in $ADObject)
-                    {
+                    FOREACH ($Account in $ADObject) {
                         Write-Verbose -Message "[PROCESS] $($Account.name)"
                         $Account | Select-Object -ExpandProperty memberof | ForEach-Object -Process {
 
                             $CurrentObject = Get-Adobject -LDAPFilter "(|(anr=$_)(distinguishedname=$_))" -Properties Samaccountname
 
 
-                            Write-Output $CurrentObject | Select-Object Name,SamAccountName,ObjectClass, @{L="Child";E={$Account.samaccountname}}
+                            Write-Output $CurrentObject | Select-Object Name, SamAccountName, ObjectClass, @{L = "Child"; E = { $Account.samaccountname } }
 
                             Write-Verbose -Message "Inception - $($CurrentObject.distinguishedname)"
                             Get-ParentGroup -OutBuffer $CurrentObject.distinguishedname
@@ -72,12 +65,11 @@
                 }#ELSE
             }#FOREACH ($Obj in $Object)
         }#TRY
-        CATCH{
+        CATCH {
             $PSCmdlet.ThrowTerminatingError($_)
         }
     }#PROCESS
-    END
-    {
+    END {
         Write-Verbose -Message "[END] Get-NestedMember"
     }
 }
