8674c0f9e8ce642d3acb077dd06023f9a53e8be7
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-30 19:14:56 -0800

    Update script format

--- SCSM-Get-SCSMWorkItemAssignedUser/Get-SCSMWorkItemAssignedUser.ps1
+++ SCSM-Get-SCSMWorkItemAssignedUser/Get-SCSMWorkItemAssignedUser.ps1
@@ -1,6 +1,5 @@
-﻿function Get-SCSMWorkItemAssignedUser
-{
-<#
+function Get-SCSMWorkItemAssignedUser {
+    <#
 	.SYNOPSIS
 		Function to retrieve the Assigned User of a Work Item
 
@@ -28,49 +27,43 @@
 		lazywinadmin.com
 #>
 
-	[CmdletBinding(DefaultParameterSetName = 'GUID')]
-	param
-	(
-		[Parameter(ParameterSetName = 'SMObject',
-				   Mandatory = $true,
-				   ValueFromPipeline = $true)]
-		$SMObject,
+    [CmdletBinding(DefaultParameterSetName = 'GUID')]
+    param
+    (
+        [Parameter(ParameterSetName = 'SMObject',
+            Mandatory = $true,
+            ValueFromPipeline = $true)]
+        $SMObject,
 
-		[Parameter(ParameterSetName = 'GUID',
-				   Mandatory = $true)]
-		$Guid
-	)
+        [Parameter(ParameterSetName = 'GUID',
+            Mandatory = $true)]
+        $Guid
+    )
 
-	BEGIN
-	{
-		Import-Module -Name SMLets -ErrorAction Stop
+    BEGIN {
+        Import-Module -Name SMLets -ErrorAction Stop
 
-		# AssignedUser RelationshipClass
-		$RelationshipClass_AssignedUser_Object = Get-SCSMRelationshipClass -Name System.WorkItemAssignedToUser$
-	}
-	PROCESS
-	{
-		IF ($PSBoundParameters['GUID'])
-		{
-			foreach ($Item in $GUID)
-			{
-				$SMObject = Get-SCSMObject -id $item
-				Write-Verbose -Message "[PROCESS] Working on $($Item.Name)"
-				Get-ScsmRelatedObject -SMObject $SMObject -Relationship $RelationshipClass_AssignedUser_Object |
-				Select-Object -Property @{ Label = "WorkItemName"; Expression = { $SMObject.Name } },
-							  @{ Label = "WorkItemGUID"; Expression = { $SMObject.get_id() } }, *
-			}
-		}
+        # AssignedUser RelationshipClass
+        $RelationshipClass_AssignedUser_Object = Get-SCSMRelationshipClass -Name System.WorkItemAssignedToUser$
+    }
+    PROCESS {
+        IF ($PSBoundParameters['GUID']) {
+            foreach ($Item in $GUID) {
+                $SMObject = Get-SCSMObject -id $item
+                Write-Verbose -Message "[PROCESS] Working on $($Item.Name)"
+                Get-ScsmRelatedObject -SMObject $SMObject -Relationship $RelationshipClass_AssignedUser_Object |
+                    Select-Object -Property @{ Label = "WorkItemName"; Expression = { $SMObject.Name } },
+                    @{ Label = "WorkItemGUID"; Expression = { $SMObject.get_id() } }, *
+            }
+        }
 
-		IF ($PSBoundParameters['SMobject'])
-		{
-			foreach ($Item in $SMObject)
-			{
-				Write-Verbose -Message "[PROCESS] Working on $($Item.Name)"
-				Get-ScsmRelatedObject -SMObject $Item -Relationship $RelationshipClass_AssignedUser_Object |
-				Select-Object -Property @{ Label = "WorkItemName"; Expression = { $Item.Name } },
-							  @{ Label = "WorkItemGUID"; Expression = { $Item.get_id() } }, *
-			}
-		}
-	}
+        IF ($PSBoundParameters['SMobject']) {
+            foreach ($Item in $SMObject) {
+                Write-Verbose -Message "[PROCESS] Working on $($Item.Name)"
+                Get-ScsmRelatedObject -SMObject $Item -Relationship $RelationshipClass_AssignedUser_Object |
+                    Select-Object -Property @{ Label = "WorkItemName"; Expression = { $Item.Name } },
+                    @{ Label = "WorkItemGUID"; Expression = { $Item.get_id() } }, *
+            }
+        }
+    }
 }
