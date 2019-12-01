4a77771e2dd348cd443bf6ceee64f52908d9df31
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-30 19:13:18 -0800

    Update script format

--- SCSM-Add-SCSMSRComment/Add-SCSMSRComment.ps1
+++ SCSM-Add-SCSMSRComment/Add-SCSMSRComment.ps1
@@ -58,27 +58,21 @@ Function Add-SCSMSRComment {
 
         [Switch]$IsPrivate
     )
-    BEGIN{
-        TRY
-        {
-            if (-not (Get-Module -Name Smlets))
-            {
+    BEGIN {
+        TRY {
+            if (-not (Get-Module -Name Smlets)) {
                 Import-Module -Name Smlets -ErrorAction 'Stop'
             }
         }
-        CATCH
-        {
+        CATCH {
             $PSCmdlet.ThrowTerminatingError($_)
         }
     }
-    PROCESS{
-        TRY
-        {
+    PROCESS {
+        TRY {
             # Make sure that the SR Object it passed to the function
-            If ($null -eq $ServiceRequestObject.Id)
-            {
-                Switch ($CommentType)
-                {
+            If ($null -eq $ServiceRequestObject.Id) {
+                Switch ($CommentType) {
                     "Analyst" {
                         $CommentClass = "System.WorkItem.TroubleTicket.AnalystCommentLog"
                         #$CommentClassName = "AnalystCommentLog"
@@ -93,17 +87,17 @@ Function Add-SCSMSRComment {
 
                 # Create the object projection with properties
                 $Projection = @{
-                    __CLASS = "System.WorkItem.ServiceRequest";
-                    __SEED = $ServiceRequestObject;
+                    __CLASS           = "System.WorkItem.ServiceRequest";
+                    __SEED            = $ServiceRequestObject;
                     EndUserCommentLog = @{
-                        __CLASS = $CommentClass;
+                        __CLASS  = $CommentClass;
                         __OBJECT = @{
-                            Id = $NewGUID;
+                            Id          = $NewGUID;
                             DisplayName = $NewGUID;
-                            Comment = $Comment;
-                            EnteredBy = $EnteredBy;
+                            Comment     = $Comment;
+                            EnteredBy   = $EnteredBy;
                             EnteredDate = (Get-Date).ToUniversalTime();
-                            IsPrivate = $IsPrivate.ToBool();
+                            IsPrivate   = $IsPrivate.ToBool();
                         }
                     }
                 }
@@ -111,13 +105,11 @@ Function Add-SCSMSRComment {
                 # Create the actual comment
                 New-SCSMObjectProjection -Type "System.WorkItem.ServiceRequestProjection" -Projection $Projection
             }
-            else
-            {
+            else {
                 Throw "Invalid Service Request Object!"
             }
         }
-        CATCH
-        {
+        CATCH {
             $PSCmdlet.ThrowTerminatingError($_)
         } #CATCH
     } #PROCESS
