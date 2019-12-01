0581efca021d9172d83cbda9a4841d60a863dc92
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-30 19:14:08 -0800

    Update script format

--- SCSM-Get-SCSMObjectPrefix/Get-SCSMObjectPrefix.ps1
+++ SCSM-Get-SCSMObjectPrefix/Get-SCSMObjectPrefix.ps1
@@ -1,6 +1,5 @@
-function Get-SCSMObjectPrefix
-{
-<#
+function Get-SCSMObjectPrefix {
+    <#
 	.SYNOPSIS
 		Function to retrieve the Prefix used for the different WorkItem, Activities or Knowledge Article
 
@@ -38,71 +37,67 @@ function Get-SCSMObjectPrefix
 		github.com/lazywinadmin
 #>
 
-	[OutputType([psobject])]
-	param
-	(
-		[ValidateSet(
-			   'DependentActivity',
-			   'ManualActivity',
-			   'ParallelActivity',
-			   'ReviewActivity',
-			   'RunbookAutomationActivity',
-			   'SequentialActivity',
-			   'IncidentRequest',
-			   'ServiceRequest',
-			   'Change',
-			   'Knowledge',
-			   'Problem',
-			   'Release'
-	)]
-		[string]$ClassName
-	)
+    [OutputType([psobject])]
+    param
+    (
+        [ValidateSet(
+            'DependentActivity',
+            'ManualActivity',
+            'ParallelActivity',
+            'ReviewActivity',
+            'RunbookAutomationActivity',
+            'SequentialActivity',
+            'IncidentRequest',
+            'ServiceRequest',
+            'Change',
+            'Knowledge',
+            'Problem',
+            'Release'
+        )]
+        [string]$ClassName
+    )
 
-	BEGIN
-	{
-		Import-Module -Name Smlets
+    BEGIN {
+        Import-Module -Name Smlets
 
-		$ActivitySettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.ActivitySettings")
-		$ChangeSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.ChangeSettings")
-		$KnowledgedSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.KnowledgeSettings")
-		$ProblemSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.ProblemSettings")
-		$ReleaseSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.ReleaseSettings")
-		$ServiceRequestSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.ServiceRequestSettings")
-		$IncidentRequestSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.WorkItem.Incident.GeneralSetting")
-	}
-	PROCESS
-	{
-		Switch ($ClassName)
-		{
-			"DependentActivity" { $ActivitySettingsObj.SystemWorkItemActivityDependentActivityIdPrefix }
-			"ManualActivity" { $ActivitySettingsObj.SystemWorkItemActivityManualActivityIdPrefix }
-			"ParallelActivity" { $ActivitySettingsObj.SystemWorkItemActivityParallelActivityIdPrefix }
-			"ReviewActivity" { $ActivitySettingsObj.SystemWorkItemActivityReviewActivityIdPrefix }
-			"RunbookAutomationActivity" { $ActivitySettingsObj.MicrosoftSystemCenterOrchestratorRunbookAutomationActivityBaseIdPrefix }
-			"SequentialActivity" { $ActivitySettingsObj.SystemWorkItemActivitySequentialActivityIdPrefix }
-			"IncidentRequest" { $IncidentRequestSettingsObj.PrefixForId }
-			"ServiceRequest" { $ServiceRequestSettingsObj.ServiceRequestPrefix }
-			"Change" { $ChangeSettingsObj.SystemWorkItemChangeRequestIdPrefix }
-			"Knowledge" { $KnowledgedSettingsObj.SystemKnowledgeArticleIdPrefix }
-			"Problem" { $ProblemSettingsObj.ProblemIdPrefix }
-			"Release" { $ReleaseSettingsObj.SystemWorkItemReleaseRecordIdPrefix }
-			default
-			{
-				[pscustomobject][ordered]@{
-					"DependentActivity" = $ActivitySettingsObj.SystemWorkItemActivityDependentActivityIdPrefix
-					"ManualActivity" = $ActivitySettingsObj.SystemWorkItemActivityManualActivityIdPrefix
-					"ParallelActivity" = $ActivitySettingsObj.SystemWorkItemActivityParallelActivityIdPrefix
-					"ReviewActivity" = $ActivitySettingsObj.SystemWorkItemActivityReviewActivityIdPrefix
-					"RunbookAutomationActivity" = $ActivitySettingsObj.MicrosoftSystemCenterOrchestratorRunbookAutomationActivityBaseIdPrefix
-					"SequentialActivity" = $ActivitySettingsObj.SystemWorkItemActivitySequentialActivityIdPrefix
-					"IncidentRequest" = $IncidentRequestSettingsObj.PrefixForId
-					"ServiceRequest" = $ServiceRequestSettingsObj.ServiceRequestPrefix
-					"Change" = $ChangeSettingsObj.SystemWorkItemChangeRequestIdPrefix
-					"Knowledge" = $KnowledgedSettingsObj.SystemKnowledgeArticleIdPrefix
-					"Problem" = $ProblemSettingsObj.ProblemIdPrefix
-					"Release" = $ReleaseSettingsObj.SystemWorkItemReleaseRecordIdPrefix
-				}
-			}
-		}
-	}
+        $ActivitySettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.ActivitySettings")
+        $ChangeSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.ChangeSettings")
+        $KnowledgedSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.KnowledgeSettings")
+        $ProblemSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.ProblemSettings")
+        $ReleaseSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.ReleaseSettings")
+        $ServiceRequestSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.GlobalSetting.ServiceRequestSettings")
+        $IncidentRequestSettingsObj = Get-SCSMObject -Class (Get-SCSMClass -Name "System.WorkItem.Incident.GeneralSetting")
+    }
+    PROCESS {
+        Switch ($ClassName) {
+            "DependentActivity" { $ActivitySettingsObj.SystemWorkItemActivityDependentActivityIdPrefix }
+            "ManualActivity" { $ActivitySettingsObj.SystemWorkItemActivityManualActivityIdPrefix }
+            "ParallelActivity" { $ActivitySettingsObj.SystemWorkItemActivityParallelActivityIdPrefix }
+            "ReviewActivity" { $ActivitySettingsObj.SystemWorkItemActivityReviewActivityIdPrefix }
+            "RunbookAutomationActivity" { $ActivitySettingsObj.MicrosoftSystemCenterOrchestratorRunbookAutomationActivityBaseIdPrefix }
+            "SequentialActivity" { $ActivitySettingsObj.SystemWorkItemActivitySequentialActivityIdPrefix }
+            "IncidentRequest" { $IncidentRequestSettingsObj.PrefixForId }
+            "ServiceRequest" { $ServiceRequestSettingsObj.ServiceRequestPrefix }
+            "Change" { $ChangeSettingsObj.SystemWorkItemChangeRequestIdPrefix }
+            "Knowledge" { $KnowledgedSettingsObj.SystemKnowledgeArticleIdPrefix }
+            "Problem" { $ProblemSettingsObj.ProblemIdPrefix }
+            "Release" { $ReleaseSettingsObj.SystemWorkItemReleaseRecordIdPrefix }
+            default {
+                [pscustomobject][ordered]@{
+                    "DependentActivity"         = $ActivitySettingsObj.SystemWorkItemActivityDependentActivityIdPrefix
+                    "ManualActivity"            = $ActivitySettingsObj.SystemWorkItemActivityManualActivityIdPrefix
+                    "ParallelActivity"          = $ActivitySettingsObj.SystemWorkItemActivityParallelActivityIdPrefix
+                    "ReviewActivity"            = $ActivitySettingsObj.SystemWorkItemActivityReviewActivityIdPrefix
+                    "RunbookAutomationActivity" = $ActivitySettingsObj.MicrosoftSystemCenterOrchestratorRunbookAutomationActivityBaseIdPrefix
+                    "SequentialActivity"        = $ActivitySettingsObj.SystemWorkItemActivitySequentialActivityIdPrefix
+                    "IncidentRequest"           = $IncidentRequestSettingsObj.PrefixForId
+                    "ServiceRequest"            = $ServiceRequestSettingsObj.ServiceRequestPrefix
+                    "Change"                    = $ChangeSettingsObj.SystemWorkItemChangeRequestIdPrefix
+                    "Knowledge"                 = $KnowledgedSettingsObj.SystemKnowledgeArticleIdPrefix
+                    "Problem"                   = $ProblemSettingsObj.ProblemIdPrefix
+                    "Release"                   = $ReleaseSettingsObj.SystemWorkItemReleaseRecordIdPrefix
+                }
+            }
+        }
+    }
 }
