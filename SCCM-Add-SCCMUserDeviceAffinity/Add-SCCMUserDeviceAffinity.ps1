7eadc296bcbccb6ad2b05f51e05e99b3c85009c5
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-30 16:25:45 -0800

    Update script format

--- SCCM-Add-SCCMUserDeviceAffinity/Add-SCCMUserDeviceAffinity.ps1
+++ SCCM-Add-SCCMUserDeviceAffinity/Add-SCCMUserDeviceAffinity.ps1
@@ -1,6 +1,5 @@
-﻿Function Add-SCCMUserDeviceAffinity
-{
-<#
+Function Add-SCCMUserDeviceAffinity {
+    <#
     .SYNOPSIS
         Function to add a primary user on a device
 
@@ -56,24 +55,21 @@
     )
 
     $Splatting = @{
-        NameSpace = "root\sms\site_$SiteCode"
+        NameSpace    = "root\sms\site_$SiteCode"
         ComputerName = $SiteServer
     }
 
-    IF ($PSBoundParameters['Credential'])
-    {
+    IF ($PSBoundParameters['Credential']) {
         $Splatting.Credential = $Credential
     }
 
 
     $AffinityType = 2 # Administrator defined
 
-    IF ($PSBoundParameters['DeviceName'])
-    {
+    IF ($PSBoundParameters['DeviceName']) {
         $ResourceID = (Get-WmiObject @Splatting -Class "SMS_CombinedDeviceResources" -Filter "Name='$DeviceName'" -ErrorAction STOP).resourceID
     }
-    IF ($PSBoundParameters['DeviceID'])
-    {
+    IF ($PSBoundParameters['DeviceID']) {
         $ResourceID = $DeviceID
     }
 
