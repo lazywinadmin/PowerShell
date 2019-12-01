cdc60f2e4e7da6291753c2223c85ce6d4d86e115
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-30 16:24:50 -0800

    Update script format

--- O365-GROUP-Get-DistributionGroupRecursive/O365-GROUP-Get-DistributionGroupRecursive.ps1
+++ O365-GROUP-Get-DistributionGroupRecursive/O365-GROUP-Get-DistributionGroupRecursive.ps1
@@ -1,4 +1,4 @@
-﻿
+
 function Get-DistributionGroupMemberRecursive {
     <#
 .SYNOPSIS
@@ -17,7 +17,7 @@ function Get-DistributionGroupMemberRecursive {
             # Retrieve Group information
             Write-Verbose -Message "[BEGIN] Retrieving members of $Group"
             $GroupMembers = Get-DistributionGroupMember -Identity $Group -ErrorAction Stop -ErrorVariable ErrorBeginGetDistribMembers |
-            Select-object -Property Name, PrimarySMTPAddress, @{ Label = "Group"; Expression = { $Group } }, RecipientType
+                Select-Object -Property Name, PrimarySMTPAddress, @{ Label = "Group"; Expression = { $Group } }, RecipientType
 
         }
         CATCH {
@@ -28,18 +28,18 @@ function Get-DistributionGroupMemberRecursive {
     PROCESS {
         FOREACH ($Member in $GroupMembers) {
             TRY {
-                Write-verbose "[PROCESS] Member: $($member.name)"
+                Write-Verbose "[PROCESS] Member: $($member.name)"
 
                 SWITCH ($Member.RecipientType) {
                     "MailUniversalDistributionGroup" {
                         # Member's type is Distribution Group, we need to find members of this object
                         Get-DistributionGroupMemberRecursive -Group $($Member.name) |
-                        Select-Object -Property Name, PrimarySMTPAddress, @{ Label = "Group"; Expression = { $($Member.name) } }, RecipientType
+                            Select-Object -Property Name, PrimarySMTPAddress, @{ Label = "Group"; Expression = { $($Member.name) } }, RecipientType
                         Write-Verbose -Message "[PROCESS] $($Member.name)"
                     }
                     "UserMailbox" {
                         # Member's type is User, let's just output the data
-                        $Member | Select-object -Property Name, PrimarySMTPAddress, @{ Label = "Group"; Expression = { $Group } }
+                        $Member | Select-Object -Property Name, PrimarySMTPAddress, @{ Label = "Group"; Expression = { $Group } }
                     }
                 }
             }
