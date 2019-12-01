447f2a6551fdb32067481ab5e0d216ae6ed8436d
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-30 19:14:28 -0800

    Update script format

--- SCSM-Get-SCSMServiceRequestComment/Get-SCSMServiceRequestComment.ps1
+++ SCSM-Get-SCSMServiceRequestComment/Get-SCSMServiceRequestComment.ps1
@@ -1,6 +1,5 @@
-﻿function Get-SCSMServiceRequestComment
-{
-<#
+function Get-SCSMServiceRequestComment {
+    <#
 	.SYNOPSIS
 		Function to retrieve the comments from a Service Request WorkItem
 
@@ -28,48 +27,45 @@
 		@lazywinadmin
 #>
 
-	PARAM
-	(
-		[Parameter(ParameterSetName = 'General',
-				   Mandatory = $true)]
-		$DateTime = $((Get-Date).AddHours(-24)),
+    PARAM
+    (
+        [Parameter(ParameterSetName = 'General',
+            Mandatory = $true)]
+        $DateTime = $((Get-Date).AddHours(-24)),
 
-		[Parameter(ParameterSetName = 'GUID')]
-		$GUID
-	)
+        [Parameter(ParameterSetName = 'GUID')]
+        $GUID
+    )
 
-	IF ($PSBoundParameters['GUID'])
-	{
-		$Tickets = Get-SCSMObject -id $GUID
-	}
-	ELSE
-	{
-		if ($DateTime -is [String]){ $DateTime = Get-Date $DateTime}
-		$DateTime = $DateTime.ToString(“yyy-MM-dd HH:mm:ss”)
-		$Tickets = Get-SCSMObject -Class (Get-SCSMClass System.WorkItem.servicerequest$) -Filter "CreatedDate -gt '$DateTime'" #| Where-Object { $_.AssignedTo -eq $NULL }
-	}
+    IF ($PSBoundParameters['GUID']) {
+        $Tickets = Get-SCSMObject -id $GUID
+    }
+    ELSE {
+        if ($DateTime -is [String]) { $DateTime = Get-Date $DateTime }
+        $DateTime = $DateTime.ToString(“yyy-MM-dd HH:mm:ss”)
+        $Tickets = Get-SCSMObject -Class (Get-SCSMClass System.WorkItem.servicerequest$) -Filter "CreatedDate -gt '$DateTime'" #| Where-Object { $_.AssignedTo -eq $NULL }
+    }
 
-	$Tickets |
-	ForEach-Object {
-		$CurrentTicket = $_
-		$relatedObjects = Get-scsmrelatedobject -SMObject $CurrentTicket
-		Foreach ($Comment in ($relatedObjects | Where-Object { $_.classname -eq 'System.WorkItem.TroubleTicket.UserCommentLog' -or $_.classname -eq 'System.WorkItem.TroubleTicket.AnalystCommentLog' -or $_.classname -eq 'System.WorkItem.TroubleTicket.AuditCommentLog'}))
-		{
-			# Output the information
-			[pscustomobject][ordered] @{
-				TicketName = $CurrentTicket.Name
-				TicketClassName = $CurrentTicket.Classname
-				TicketDisplayName = $CurrentTicket.DisplayName
-				TicketID = $CurrentTicket.ID
-				TicketGUID = $CurrentTicket.get_id()
-				TicketSupportGroup = $CurrentTicket.SupportGroup.displayname
-				TicketAssignedTo = $CurrentTicket.AssignedTo
-				TicketCreatedDate = $CurrentTicket.CreatedDate
-				Comment = $Comment.Comment
-				CommentEnteredBy = $Comment.EnteredBy
-				CommentEnteredDate = $Comment.EnteredDate
-				CommentClassName = $Comment.ClassName
-			}
-		}
-	}
+    $Tickets |
+        ForEach-Object {
+            $CurrentTicket = $_
+            $relatedObjects = Get-scsmrelatedobject -SMObject $CurrentTicket
+            Foreach ($Comment in ($relatedObjects | Where-Object { $_.classname -eq 'System.WorkItem.TroubleTicket.UserCommentLog' -or $_.classname -eq 'System.WorkItem.TroubleTicket.AnalystCommentLog' -or $_.classname -eq 'System.WorkItem.TroubleTicket.AuditCommentLog' })) {
+                # Output the information
+                [pscustomobject][ordered] @{
+                    TicketName         = $CurrentTicket.Name
+                    TicketClassName    = $CurrentTicket.Classname
+                    TicketDisplayName  = $CurrentTicket.DisplayName
+                    TicketID           = $CurrentTicket.ID
+                    TicketGUID         = $CurrentTicket.get_id()
+                    TicketSupportGroup = $CurrentTicket.SupportGroup.displayname
+                    TicketAssignedTo   = $CurrentTicket.AssignedTo
+                    TicketCreatedDate  = $CurrentTicket.CreatedDate
+                    Comment            = $Comment.Comment
+                    CommentEnteredBy   = $Comment.EnteredBy
+                    CommentEnteredDate = $Comment.EnteredDate
+                    CommentClassName   = $Comment.ClassName
+                }
+            }
+        }
 }
