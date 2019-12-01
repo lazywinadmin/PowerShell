52891aa8c3360c39ef1ee69daba8d36d48c8087a
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-30 19:19:23 -0800

    Update script format

--- TOOL-Clean-MacAddress/Clean-MacAddress.ps1
+++ TOOL-Clean-MacAddress/Clean-MacAddress.ps1
@@ -1,6 +1,5 @@
-function Clean-MacAddress
-{
-<#
+function Clean-MacAddress {
+    <#
 	.SYNOPSIS
 		Function to cleanup a MACAddress string
 
@@ -56,58 +55,51 @@ function Clean-MacAddress
 		lazywinadmin.com
 		@lazywinadmin
 #>
-	[OutputType([String], ParameterSetName = "Upper")]
-	[OutputType([String], ParameterSetName = "Lower")]
-	[CmdletBinding(DefaultParameterSetName = 'Upper')]
-	param
-	(
-		[Parameter(ParameterSetName = 'Lower')]
-		[Parameter(ParameterSetName = 'Upper')]
-		[String]$MacAddress,
+    [OutputType([String], ParameterSetName = "Upper")]
+    [OutputType([String], ParameterSetName = "Lower")]
+    [CmdletBinding(DefaultParameterSetName = 'Upper')]
+    param
+    (
+        [Parameter(ParameterSetName = 'Lower')]
+        [Parameter(ParameterSetName = 'Upper')]
+        [String]$MacAddress,
 
-		[Parameter(ParameterSetName = 'Lower')]
-		[Parameter(ParameterSetName = 'Upper')]
-		[ValidateSet(':', 'None', '.', "-")]
-		$Separator,
+        [Parameter(ParameterSetName = 'Lower')]
+        [Parameter(ParameterSetName = 'Upper')]
+        [ValidateSet(':', 'None', '.', "-")]
+        $Separator,
 
-		[Parameter(ParameterSetName = 'Upper')]
-		[Switch]$Uppercase,
+        [Parameter(ParameterSetName = 'Upper')]
+        [Switch]$Uppercase,
 
-		[Parameter(ParameterSetName = 'Lower')]
-		[Switch]$Lowercase
-	)
+        [Parameter(ParameterSetName = 'Lower')]
+        [Switch]$Lowercase
+    )
 
-	BEGIN
-	{
-		# Initial Cleanup
-		$MacAddress = $MacAddress -replace "-", "" #Replace Dash
-		$MacAddress = $MacAddress -replace ":", "" #Replace Colon
-		$MacAddress = $MacAddress -replace "/s", "" #Remove whitespace
-		$MacAddress = $MacAddress -replace " ", "" #Remove whitespace
-		$MacAddress = $MacAddress -replace "\.", "" #Remove dots
-		$MacAddress = $MacAddress.trim() #Remove space at the beginning
-		$MacAddress = $MacAddress.trimend() #Remove space at the end
-	}
-	PROCESS
-	{
-		IF ($PSBoundParameters['Uppercase'])
-		{
-			$MacAddress = $macaddress.toupper()
-		}
-		IF ($PSBoundParameters['Lowercase'])
-		{
-			$MacAddress = $macaddress.tolower()
-		}
-		IF ($PSBoundParameters['Separator'])
-		{
-			IF ($Separator -ne "None")
-			{
-				$MacAddress = $MacAddress -replace '(..(?!$))', "`$1$Separator"
-			}
-		}
-	}
-	END
-	{
-		Write-Output $MacAddress
-	}
+    BEGIN {
+        # Initial Cleanup
+        $MacAddress = $MacAddress -replace "-", "" #Replace Dash
+        $MacAddress = $MacAddress -replace ":", "" #Replace Colon
+        $MacAddress = $MacAddress -replace "/s", "" #Remove whitespace
+        $MacAddress = $MacAddress -replace " ", "" #Remove whitespace
+        $MacAddress = $MacAddress -replace "\.", "" #Remove dots
+        $MacAddress = $MacAddress.trim() #Remove space at the beginning
+        $MacAddress = $MacAddress.trimend() #Remove space at the end
+    }
+    PROCESS {
+        IF ($PSBoundParameters['Uppercase']) {
+            $MacAddress = $macaddress.toupper()
+        }
+        IF ($PSBoundParameters['Lowercase']) {
+            $MacAddress = $macaddress.tolower()
+        }
+        IF ($PSBoundParameters['Separator']) {
+            IF ($Separator -ne "None") {
+                $MacAddress = $MacAddress -replace '(..(?!$))', "`$1$Separator"
+            }
+        }
+    }
+    END {
+        Write-Output $MacAddress
+    }
 }
