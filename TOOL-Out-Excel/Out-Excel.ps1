dbf813354cb393169b538a4d53e6a6c9b4ff87f9
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-12-03 16:02:21 -0800

    Update script format

--- TOOL-Out-Excel/Out-Excel.ps1
+++ TOOL-Out-Excel/Out-Excel.ps1
@@ -1,6 +1,5 @@
-﻿function Out-Excel
-{
-<#
+function Out-Excel {
+    <#
 .SYNOPSIS
     Convert PowerShellObject to Excel file
 .DESCRIPTION
@@ -27,8 +26,7 @@ TODO:
         [switch]$raw
     )
 
-    BEGIN
-    {
+    BEGIN {
         # start Excel and open a new workbook
         $Excel = New-Object -Com Excel.Application
         $Excel.visible = $True
@@ -40,35 +38,29 @@ TODO:
         $HeaderHash = @{ }
     }
 
-    PROCESS
-    {
+    PROCESS {
         if ($_ -eq $null) { return }
-        if ($Row -eq 1)
-        {
+        if ($Row -eq 1) {
             # when we see the first object, we need to build our header table
-            if (-not $property)
-            {
+            if (-not $property) {
                 # if we haven’t been provided a list of properties,
                 # we’ll build one from the object’s properties
                 $property = @()
-                if ($raw)
-                {
+                if ($raw) {
                     $_.properties.PropertyNames |
-                    ForEach-Object -Process {
-                        $property += @($_)
-                    }
+                        ForEach-Object -Process {
+                            $property += @($_)
+                        }
                 }
-                else
-                {
+                else {
                     $_.PsObject.get_properties() |
-                    ForEach-Object -Process {
-                        $property += @($_.Name.ToString())
-                    }
+                        ForEach-Object -Process {
+                            $property += @($_.Name.ToString())
+                        }
                 }
             }
             $Column = 1
-            foreach ($header in $property)
-            {
+            foreach ($header in $property) {
                 # iterate through the property list and load the headers into the first row
                 # also build a hash table so we can retrieve the correct column number
                 # when we process each object
@@ -84,28 +76,23 @@ TODO:
             $WorkBook.HorizontalAlignment = -4108
         }
         $Row++
-        foreach ($header in $property)
-        {
+        foreach ($header in $property) {
             # now for each object we can just enumerate the headers, find the matching property
             # and load the data into the correct cell in the current row.
             # this way we don’t have to worry about missing properties
             # or the “ordering” of the properties
-            if ($thisColumn = $HeaderHash[$header])
-            {
-                if ($raw)
-                {
+            if ($thisColumn = $HeaderHash[$header]) {
+                if ($raw) {
                     $Sheet.Cells.Item($Row, $thisColumn) = [string]$_.properties.$header
                 }
-                else
-                {
+                else {
                     $Sheet.Cells.Item($Row, $thisColumn) = [string]$_.$header
                 }
             }
         }
     }
 
-    end
-    {
+    end {
         # now just resize the columns and we’re finished
         if ($Row -gt 1) { [void]$WorkBook.EntireColumn.AutoFit() }
     }
