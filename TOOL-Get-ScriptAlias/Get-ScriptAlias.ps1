d475129c493898c57c79ee0de5835d0300d57af9
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-12-03 16:00:46 -0800

    Update script format

--- TOOL-Get-ScriptAlias/Get-ScriptAlias.ps1
+++ TOOL-Get-ScriptAlias/Get-ScriptAlias.ps1
@@ -1,6 +1,5 @@
-﻿function Get-ScriptAlias
-{
-<#
+function Get-ScriptAlias {
+    <#
 	.SYNOPSIS
 		Function to retrieve the aliases inside a Powershell script file.
 
@@ -25,54 +24,49 @@
 		lazywinadmin.com
 		@lazywinadmin
 #>
-	[CmdletBinding()]
-	PARAM
-	(
-		[Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
-		[ValidateScript({ Test-Path -Path $_ })]
-		[Alias("FullName")]
-		[System.String[]]$Path
-	)
-	PROCESS
-	{
-		FOREACH ($File in $Path)
-		{
-			TRY
-			{
-				# Retrieve file content
-				$ScriptContent = (Get-Content $File -Delimiter $([char]0))
+    [CmdletBinding()]
+    PARAM
+    (
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [ValidateScript( { Test-Path -Path $_ })]
+        [Alias("FullName")]
+        [System.String[]]$Path
+    )
+    PROCESS {
+        FOREACH ($File in $Path) {
+            TRY {
+                # Retrieve file content
+                $ScriptContent = (Get-Content $File -Delimiter $([char]0))
 
-				# AST Parsing
-				$AbstractSyntaxTree = [System.Management.Automation.Language.Parser]::
-				ParseInput($ScriptContent, [ref]$null, [ref]$null)
+                # AST Parsing
+                $AbstractSyntaxTree = [System.Management.Automation.Language.Parser]::
+                ParseInput($ScriptContent, [ref]$null, [ref]$null)
 
-				# Find Aliases
-				$AbstractSyntaxTree.FindAll({ $args[0] -is [System.Management.Automation.Language.CommandAst] }, $true) |
-				ForEach-Object -Process {
-					$Command = $_.CommandElements[0]
-					if ($Alias = Get-Alias | Where-Object { $_.Name -eq $Command })
-					{
+                # Find Aliases
+                $AbstractSyntaxTree.FindAll( { $args[0] -is [System.Management.Automation.Language.CommandAst] }, $true) |
+                    ForEach-Object -Process {
+                        $Command = $_.CommandElements[0]
+                        if ($Alias = Get-Alias | Where-Object { $_.Name -eq $Command }) {
 
-						# Output information
-						[PSCustomObject]@{
-							File = $File
-							Alias = $Alias.Name
-							Definition = $Alias.Definition
-							StartLineNumber = $Command.Extent.StartLineNumber
-							EndLineNumber = $Command.Extent.EndLineNumber
-							StartColumnNumber = $Command.Extent.StartColumnNumber
-							EndColumnNumber = $Command.Extent.EndColumnNumber
-							StartOffset = $Command.Extent.StartOffset
-							EndOffset = $Command.Extent.EndOffset
+                            # Output information
+                            [PSCustomObject]@{
+                                File              = $File
+                                Alias             = $Alias.Name
+                                Definition        = $Alias.Definition
+                                StartLineNumber   = $Command.Extent.StartLineNumber
+                                EndLineNumber     = $Command.Extent.EndLineNumber
+                                StartColumnNumber = $Command.Extent.StartColumnNumber
+                                EndColumnNumber   = $Command.Extent.EndColumnNumber
+                                StartOffset       = $Command.Extent.StartOffset
+                                EndOffset         = $Command.Extent.EndOffset
 
-						}#[PSCustomObject]
-					}#if ($Alias)
-				}#ForEach-Object
-			}#TRY
-			CATCH
-			{
-				Write-Error -Message $($Error[0].Exception.Message)
-			} #CATCH
-		}#FOREACH ($File in $Path)
-	} #PROCESS
+                            }#[PSCustomObject]
+                        }#if ($Alias)
+                    }#ForEach-Object
+            }#TRY
+            CATCH {
+                Write-Error -Message $($Error[0].Exception.Message)
+            } #CATCH
+        }#FOREACH ($File in $Path)
+    } #PROCESS
 }
