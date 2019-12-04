d4852f1d48fd7b6f617b41f7c0002724966050a9
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-12-03 16:06:05 -0800

    Update script format

--- _Profiles/functions/View-Cats.ps1
+++ _Profiles/functions/View-Cats.ps1
@@ -1,5 +1,4 @@
-function View-Cats
-{
+function View-Cats {
     <#
     .SYNOPSIS
         This will open Internet explorer and show a different cat every 5 seconds
@@ -8,15 +7,15 @@ function View-Cats
         #http://www.reddit.com/r/PowerShell/comments/2htfog/viewcats/
     #>
     Param(
-        [int]$refreshtime=5
+        [int]$refreshtime = 5
     )
-    $IE = new-object -ComObject internetexplorer.application
+    $IE = New-Object -ComObject internetexplorer.application
     $IE.visible = $true
     $IE.FullScreen = $true
     $shell = New-Object -ComObject wscript.shell
     $shell.AppActivate("Internet Explorer")
 
-    while($true){
+    while ($true) {
         $request = Invoke-WebRequest -Uri "http://thecatapi.com/api/images/get" -Method get
         $IE.Navigate($request.BaseResponse.ResponseUri.AbsoluteUri)
         Start-Sleep -Seconds $refreshtime
