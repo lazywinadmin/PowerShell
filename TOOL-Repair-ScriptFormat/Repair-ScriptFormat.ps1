067a355551921e57b809faee3f92c9ea548e1fd2
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-12-03 16:03:12 -0800

    Add Repair-ScriptFormat

---
+++ TOOL-Repair-ScriptFormat/Repair-ScriptFormat.ps1
@@ -0,0 +1,33 @@
+function Repair-ScriptFormat {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline)]
+        [ValidateScript( { Test-Path -Path $_ })]
+        $Path,
+        [System.String]$Settings = 'CodeFormatting'
+    )
+
+    begin {
+        #Validate PSScriptAnalyzer is present
+        #minimum vers
+    }
+
+    process {
+        # Retrieve content
+        $scriptContent = Get-Content -Path $Path -Raw
+
+        # Apply Formatting
+        $NewContent = Invoke-Formatter -ScriptDefinition $scriptContent -Settings $Settings
+
+        # Replace whitespace lines
+        #$NewContent = $NewContent -replace '\'
+
+        $NewContent | Out-File -Path $Path -Force -NoNewline
+
+
+
+    }
+    end {
+
+    }
+}
