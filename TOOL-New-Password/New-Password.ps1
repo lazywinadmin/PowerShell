f33cb2a27f75de7f4fd6a4bb44b5e3a904b7cea1
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-04 22:34:10 -0800

    Replace tabs

--- TOOL-New-Password/New-Password.ps1
+++ TOOL-New-Password/New-Password.ps1
@@ -1,39 +1,39 @@
 function New-Password {
     <#
-	.SYNOPSIS
-		Function to Generate a new password.
+    .SYNOPSIS
+        Function to Generate a new password.
 
-	.DESCRIPTION
-		Function to Generate a new password.
-		By default it will generate a 12 characters length password, you can change this using the parameter Length.
-		I excluded the following characters: ",',.,/,1,<,>,`,O,0,l,|
+    .DESCRIPTION
+        Function to Generate a new password.
+        By default it will generate a 12 characters length password, you can change this using the parameter Length.
+        I excluded the following characters: ",',.,/,1,<,>,`,O,0,l,|
         Some of those are ambiguous characters like 1 or l or |
-		You can add exclusion by checking the following ASCII Table http://www.asciitable.com/
+        You can add exclusion by checking the following ASCII Table http://www.asciitable.com/
 
-		If the length requested is less or equal to 4, it will generate a random password.
-		If the length requested is greater than 4, it will make sure the password contains an Upper and Lower case letter, a Number and a special character
+        If the length requested is less or equal to 4, it will generate a random password.
+        If the length requested is greater than 4, it will make sure the password contains an Upper and Lower case letter, a Number and a special character
 
-	.PARAMETER Length
-		Specifies the length of the password.
+    .PARAMETER Length
+        Specifies the length of the password.
         Default is 12 characters
 
     .PARAMETER Count
         Specifies how many password you want to output.
         Default is 1 password.
 
-	.EXAMPLE
-		PS C:\> New-Password -Length 30
+    .EXAMPLE
+        PS C:\> New-Password -Length 30
 
-		=E)(72&:f\W6:VRGE(,t1x6sZi-346
+        =E)(72&:f\W6:VRGE(,t1x6sZi-346
 
     .EXAMPLE
         PS C:\> New-Password 3
 
         !}R
 
-	.NOTES
-		See ASCII Table http://www.asciitable.com/
-		Code based on a blog post of https://mjolinor.wordpress.com/2014/01/31/random-password-generator/
+    .NOTES
+        See ASCII Table http://www.asciitable.com/
+        Code based on a blog post of https://mjolinor.wordpress.com/2014/01/31/random-password-generator/
 #>
     [CmdletBinding()]
     PARAM
