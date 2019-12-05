9e63e0bdb8d5ccef534af094cb4fee6a9148683c
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-04 22:32:01 -0800

    Replace tabs

--- SCSM-Get-SCSMServiceRequestComment/Get-SCSMServiceRequestComment.ps1
+++ SCSM-Get-SCSMServiceRequestComment/Get-SCSMServiceRequestComment.ps1
@@ -1,30 +1,30 @@
 function Get-SCSMServiceRequestComment {
     <#
-	.SYNOPSIS
-		Function to retrieve the comments from a Service Request WorkItem
+    .SYNOPSIS
+        Function to retrieve the comments from a Service Request WorkItem
 
-	.DESCRIPTION
-		Function to retrieve the comments from a Service Request WorkItem
+    .DESCRIPTION
+        Function to retrieve the comments from a Service Request WorkItem
 
-	.PARAMETER DateTime
-		Specifies from when (DateTime) the search need to look
+    .PARAMETER DateTime
+        Specifies from when (DateTime) the search need to look
 
-	.PARAMETER GUID
-		Specifies the GUID of the Service Request or Incident
+    .PARAMETER GUID
+        Specifies the GUID of the Service Request or Incident
 
-	.EXAMPLE
-		Get-SCSMServiceRequestComment -DateTime $((Get-Date).AddHours(-15))
+    .EXAMPLE
+        Get-SCSMServiceRequestComment -DateTime $((Get-Date).AddHours(-15))
 
-	.EXAMPLE
-		Get-SCSMServiceRequestComment -DateTime "2016/01/01"
+    .EXAMPLE
+        Get-SCSMServiceRequestComment -DateTime "2016/01/01"
 
-	.EXAMPLE
-		Get-SCSMServiceRequestComment -GUID 221dbd07-b480-ee33-fc25-6077406e83ad
+    .EXAMPLE
+        Get-SCSMServiceRequestComment -GUID 221dbd07-b480-ee33-fc25-6077406e83ad
 
-	.NOTES
-		Francois-Xavier Cat
-		lazywinadmin.com
-		@lazywinadmin
+    .NOTES
+        Francois-Xavier Cat
+        lazywinadmin.com
+        @lazywinadmin
 #>
 
     PARAM
