bd6fb02481b8aa61c1c943d52f776eac36ac51b8
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-04 22:32:38 -0800

    Replace tabs

--- TOOL-Get-ImageInformation/Get-ImageInformation.ps1
+++ TOOL-Get-ImageInformation/Get-ImageInformation.ps1
@@ -1,22 +1,22 @@
 function Get-ImageInformation {
     <#
 .SYNOPSIS
-	function to retrieve Image file information
+    function to retrieve Image file information
 
 .DESCRIPTION
-	function to retrieve Image file information
+    function to retrieve Image file information
 
 .PARAMETER FilePath
-	Specify one or multiple image file path(s).
+    Specify one or multiple image file path(s).
 
 .EXAMPLE
-	PS C:\> Get-ImageInformation -FilePath c:\temp\image.png
+    PS C:\> Get-ImageInformation -FilePath c:\temp\image.png
 
 .NOTES
-	Francois-Xavier Cat
-	lazywinadmin.com
-	@lazywinadmin
-	github.com/lazywinadmin
+    Francois-Xavier Cat
+    lazywinadmin.com
+    @lazywinadmin
+    github.com/lazywinadmin
 #>
     PARAM (
         [System.String[]]$FilePath
