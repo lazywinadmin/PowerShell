3f31e3404e4ffaaec23794da4acf16de5c6644fa
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-04 22:33:58 -0800

    Replace tabs

--- TOOL-Get-StringCharCount/Get-StringCharCount.ps1
+++ TOOL-Get-StringCharCount/Get-StringCharCount.ps1
@@ -1,18 +1,18 @@
 Function Get-StringCharCount {
     <#
-	.SYNOPSIS
-		This function will count the number of characters in a string
-	.DESCRIPTION
-		This function will count the number of characters in a string
-	.EXAMPLE
-		PS C:\> Get-StringCharCount -String "Hello World"
+    .SYNOPSIS
+        This function will count the number of characters in a string
+    .DESCRIPTION
+        This function will count the number of characters in a string
+    .EXAMPLE
+        PS C:\> Get-StringCharCount -String "Hello World"
 
-		11
-	.NOTES
-		Francois-Xavier Cat
-		@lazywinadmin
-		lazywinadmin.com
-	#>
+        11
+    .NOTES
+        Francois-Xavier Cat
+        @lazywinadmin
+        lazywinadmin.com
+    #>
     PARAM ([String]$String)
     ($String -as [Char[]]).count
 }
