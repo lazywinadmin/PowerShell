5e5dbdc3b94873e81d0da418d4568f6378dfbea5
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-04 22:34:25 -0800

    Replace tabs

--- TOOL-Remove-StringDiacritic/Remove-StringDiacritic.ps1
+++ TOOL-Remove-StringDiacritic/Remove-StringDiacritic.ps1
@@ -1,28 +1,28 @@
 function Remove-StringDiacritic {
     <#
 .SYNOPSIS
-	This function will remove the diacritics (accents) characters from a string.
+    This function will remove the diacritics (accents) characters from a string.
 
 .DESCRIPTION
-	This function will remove the diacritics (accents) characters from a string.
+    This function will remove the diacritics (accents) characters from a string.
 
 .PARAMETER String
-	Specifies the String(s) on which the diacritics need to be removed
+    Specifies the String(s) on which the diacritics need to be removed
 
 .PARAMETER NormalizationForm
-	Specifies the normalization form to use
-	https://msdn.microsoft.com/en-us/library/system.text.normalizationform(v=vs.110).aspx
+    Specifies the normalization form to use
+    https://msdn.microsoft.com/en-us/library/system.text.normalizationform(v=vs.110).aspx
 
 .EXAMPLE
-	PS C:\> Remove-StringDiacritic "L'été de Raphaël"
+    PS C:\> Remove-StringDiacritic "L'été de Raphaël"
 
-	L'ete de Raphael
+    L'ete de Raphael
 
 .NOTES
-	Francois-Xavier Cat
-	@lazywinadmin
-	lazywinadmin.com
-	github.com/lazywinadmin
+    Francois-Xavier Cat
+    @lazywinadmin
+    lazywinadmin.com
+    github.com/lazywinadmin
 #>
     [CMdletBinding()]
     PARAM
