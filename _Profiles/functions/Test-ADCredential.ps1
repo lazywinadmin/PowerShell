c9ebd58dbc91857b229bfe07e554dba818c0d2d9
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-12 22:02:16 -0800

    Add missing parameter

--- _Profiles/functions/Test-ADCredential.ps1
+++ _Profiles/functions/Test-ADCredential.ps1
@@ -2,8 +2,8 @@ Function Test-ADCredential {
     Param($username, $password, $domain)
     Add-Type -AssemblyName System.DirectoryServices.AccountManagement
     $ct = [System.DirectoryServices.AccountManagement.ContextType]::Domain
-    $pc = New-Object System.DirectoryServices.AccountManagement.PrincipalContext($ct, $domain)
-    New-Object PSObject -Property @{
+    $pc = New-Object -TypeName System.DirectoryServices.AccountManagement.PrincipalContext($ct, $domain)
+    New-Object -TypeName PSObject -Property @{
         UserName = $username;
         IsValid  = $pc.ValidateCredentials($username, $password).ToString()
     }
