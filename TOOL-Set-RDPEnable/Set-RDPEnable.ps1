f953654e151dea7f03bf444830d5ec948d89bfe8
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-21 21:43:13 -0800

    Add support for whatif

--- TOOL-Set-RDPEnable/Set-RDPEnable.ps1
+++ TOOL-Set-RDPEnable/Set-RDPEnable.ps1
@@ -24,20 +24,22 @@ function Set-RDPEnable {
         @lazywinadmin
 #>
 
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $true)]
     PARAM (
         [String[]]$ComputerName = $env:COMPUTERNAME
     )
     PROCESS {
         FOREACH ($Computer in $ComputerName) {
             TRY {
-                IF (Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
-                    $regKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $Computer)
-                    $regKey = $regKey.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Terminal Server", $True)
-                    $regkey.SetValue("fDenyTSConnections", 0)
-                    $regKey.flush()
-                    $regKey.Close()
-                } #IF Test-Connection
+                IF ($PSCmdlet.ShouldProcess($Computer, "Enable Remote Desktop")) {
+                    IF (Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
+                        $regKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $Computer)
+                        $regKey = $regKey.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Terminal Server", $True)
+                        $regkey.SetValue("fDenyTSConnections", 0)
+                        $regKey.flush()
+                        $regKey.Close()
+                    } #IF Test-Connection
+                }
             } #Try
             CATCH {
                 $PSCmdlet.ThrowTerminatingError($_)
