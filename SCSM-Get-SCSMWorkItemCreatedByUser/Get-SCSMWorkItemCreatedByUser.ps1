9d8ab949184966065cc6b4367a73db18617c8b5a
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-30 23:11:05 -0800

    Add link

--- SCSM-Get-SCSMWorkItemCreatedByUser/Get-SCSMWorkItemCreatedByUser.ps1
+++ SCSM-Get-SCSMWorkItemCreatedByUser/Get-SCSMWorkItemCreatedByUser.ps1
@@ -25,6 +25,8 @@ function Get-SCSMWorkItemCreatedByUser {
         Francois-Xavier Cat
         @lazywinadmin
         lazywinadmin.com
+    .LINK
+        https://github.com/lazywinadmin/PowerShell
 #>
 
     [CmdletBinding(DefaultParameterSetName = 'GUID')]
