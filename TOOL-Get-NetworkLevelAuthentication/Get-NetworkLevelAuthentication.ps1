75a83eb52e63c668aba35f6201c8391b3dce5090
lazywinadmin <fxcat@lazywinadmin.com>
2020-01-02 10:56:20 -0800

    add link

--- TOOL-Get-NetworkLevelAuthentication/Get-NetworkLevelAuthentication.ps1
+++ TOOL-Get-NetworkLevelAuthentication/Get-NetworkLevelAuthentication.ps1
@@ -56,7 +56,8 @@ function Get-NetworkLevelAuthentication {
     Twitter     : @lazywinadmin
 
     Article : http://lazywinadmin.com/2014/04/powershell-getset-network-level.html
-    GitHub    : https://github.com/lazywinadmin/PowerShell
+.LINK
+    https://github.com/lazywinadmin/PowerShell
 #>
     #Requires -Version 3.0
     [CmdletBinding()]
