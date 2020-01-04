53b3a8ed60f0519d2c67f6c1c231a973c6e3126e
lazywinadmin <fxcat@lazywinadmin.com>
2020-01-04 11:42:21 -0800

    add link

--- TOOL-Remove-PSObjectProperty/Remove-PSObjectProperty.ps1
+++ TOOL-Remove-PSObjectProperty/Remove-PSObjectProperty.ps1
@@ -1,24 +1,26 @@
 function Remove-PSObjectProperty {
-    <#
-    .SYNOPSIS
-        Function to Remove a specifid property from a PowerShell object
+<#
+.SYNOPSIS
+    Function to Remove a specifid property from a PowerShell object
 
-    .DESCRIPTION
-        Function to Remove a specifid property from a PowerShell object
+.DESCRIPTION
+    Function to Remove a specifid property from a PowerShell object
 
-    .PARAMETER PSObject
-        Specifies the PowerShell Object
+.PARAMETER PSObject
+    Specifies the PowerShell Object
 
-    .PARAMETER Property
-        Specifies the property to remove
+.PARAMETER Property
+    Specifies the property to remove
 
-    .EXAMPLE
-        PS C:\> Remove-PSObjectProperty -PSObject $UserInfo -Property Info
+.EXAMPLE
+    PS C:\> Remove-PSObjectProperty -PSObject $UserInfo -Property Info
 
-    .NOTES
-        Francois-Xavier Cat
-        lazywinadmin.com
-        @lazywinadmin
+.NOTES
+    Francois-Xavier Cat
+    lazywinadmin.com
+    @lazywinadmin
+.LINK
+    https://github.com/lazywinadmin/PowerShell
 #>
     PARAM (
         $PSObject,
